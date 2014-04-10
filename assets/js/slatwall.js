143ea88044e31d9af4fbdc2f7ef40af088b63203
JRogaishio <me@JacobRogaishio.com>
2014-04-10 09:53:17 -0400

    AngularJS testing

--- assets/js/slatwall.js
+++ assets/js/slatwall.js
@@ -1,12 +1,20 @@
 var ngSlatwall = angular.module('ng-slatwall', []);
 
-ngSlatwall.controller('admin-entity-preprocessaccount_addaccountpayment', function($scope, $filter) {
-	
+ngSlatwall.controller('admin-entity-preprocessaccount_addaccountpayment', function($scope, $compile) {
+	alert("Loaded Controller");
 	$scope.updateSubTotal = function() {
 		$scope.totalAmountToApply = 0;
+		
 		//Loop through all the amount fields and create a running subtotal
 		angular.forEach($scope.appliedOrderPayment, function(value, key){
 			$scope.totalAmountToApply += parseFloat(value);
 	    });
+	}
+	
+	$scope.recompile = function() {
+		alert("CALLED REFRESH");
+		console.log("test");
+		//$compile($('html'))($scope);
+		
 	}
 });
