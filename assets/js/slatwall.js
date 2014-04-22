d42fc8a4e4a2553104612023bdd31977648c9262
JRogaishio <me@JacobRogaishio.com>
2014-04-22 14:42:57 -0400

    Added number parsing

--- assets/js/slatwall.js
+++ assets/js/slatwall.js
@@ -3,6 +3,8 @@ var ngSlatwall = angular.module('ng-slatwall', []);
 //============= START: Controllers ===================
 
 ngSlatwall.controller('admin-entity-preprocessaccount_addaccountpayment', function($scope, $compile) {
+	//Define the different payment types used here
+	var paymentType = {aptCharge:"444df32dd2b0583d59a19f1b77869025",aptCredit:"444df32e9b448ea196c18c66e1454c46", aptAdjustment:"68e3fb57d8102b47acc0003906d16ddd"};
 	$scope.totalAmountToApply = 0; //Default value to show on new form
 	$scope.paymentTypeName = "Charge" //Default payment type
 	$scope.paymentTypeLock = true; //Used to lock down the order payment type dropdowns
@@ -14,13 +16,13 @@ ngSlatwall.controller('admin-entity-preprocessaccount_addaccountpayment', functi
 		});
 
 		
-		if($scope.paymentType=="444df32dd2b0583d59a19f1b77869025") {
+		if($scope.paymentType==paymentType.aptCharge) {
 			$scope.paymentTypeName = "Charge"
 			$scope.paymentTypeLock = true;
-		} else if($scope.paymentType=="444df32e9b448ea196c18c66e1454c46") {
+		} else if($scope.paymentType==paymentType.aptCredit) {
 			$scope.paymentTypeName = "Credit"
 			$scope.paymentTypeLock = true;
-		} else if($scope.paymentType=="68e3fb57d8102b47acc0003906d16ddd") {
+		} else if($scope.paymentType==paymentType.aptAdjustment) {
 			$scope.paymentTypeLock = false;
 			$scope.paymentTypeName = "Adjustment"
 			$scope.amount = 0;
@@ -41,17 +43,17 @@ ngSlatwall.controller('admin-entity-preprocessaccount_addaccountpayment', functi
 			//Don't count the field if its undefied or not a number
 			if(obj.amount != undefined && !isNaN(obj.amount)) {
 				//Charge / adjustment condition for subtotal
-				if($scope.paymentType=="444df32dd2b0583d59a19f1b77869025" || $scope.paymentType == '68e3fb57d8102b47acc0003906d16ddd') {
-					if(obj.paymentType=='444df32dd2b0583d59a19f1b77869025')
+				if($scope.paymentType==paymentType.aptCharge || $scope.paymentType == paymentType.aptAdjustment) {
+					if(obj.paymentType==paymentType.aptCharge)
 						$scope.totalAmountToApply += parseFloat(obj.amount);
-					else if(obj.paymentType=='444df32e9b448ea196c18c66e1454c46')
+					else if(obj.paymentType==paymentType.aptCredit)
 						$scope.totalAmountToApply -= parseFloat(obj.amount);
 
 				//Credit condition for subtotal
-				} else if($scope.paymentType=="444df32e9b448ea196c18c66e1454c46") {
-					if(obj.paymentType=='444df32dd2b0583d59a19f1b77869025')
+				} else if($scope.paymentType==paymentType.aptCredit) {
+					if(obj.paymentType==paymentType.aptCharge)
 						$scope.totalAmountToApply -= parseFloat(obj.amount);
-					else if(obj.paymentType=='444df32e9b448ea196c18c66e1454c46')
+					else if(obj.paymentType==paymentType.aptCredit)
 						$scope.totalAmountToApply += parseFloat(obj.amount);
 				}
 			}
@@ -59,14 +61,13 @@ ngSlatwall.controller('admin-entity-preprocessaccount_addaccountpayment', functi
 
 		//The amount not applied to an order
 	    $scope.amountUnapplied = (Math.round(($scope.amount - $scope.totalAmountToApply) * 100) / 100);
-		$scope.accountBalanceChange = $scope.amount;
+		$scope.accountBalanceChange = parseFloat($scope.amount);
 		
 		//Switch the account balance display amount to a negative if you are doing a charge
-		if($scope.paymentType=='444df32dd2b0583d59a19f1b77869025')
-			$scope.accountBalanceChange = $scope.accountBalanceChange * -1; //If charge, change to neg since we are lowering account balance
-		else if($scope.paymentType=='68e3fb57d8102b47acc0003906d16ddd')
-			$scope.accountBalanceChange += $scope.amountUnapplied; //If adjustment, use the amount unapplied to determine the balance change
-
+		if($scope.paymentType==paymentType.aptCharge)
+			$scope.accountBalanceChange = parseFloat($scope.accountBalanceChange * -1); //If charge, change to neg since we are lowering account balance
+		else if($scope.paymentType==paymentType.aptAdjustment)
+			$scope.accountBalanceChange += parseFloat($scope.amountUnapplied); //If adjustment, use the amount unapplied to determine the balance change
 	}
 });
 
