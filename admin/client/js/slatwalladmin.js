8ed0ae6bda08177da9817b1762fcc99abf425d68
rmarchandTen24 <ryanmarchand@192.168.1.5>
2014-08-14 09:53:57 -0400

    removing the slatwalladmin folder now path is client/js

--- admin/client/js/slatwalladmin.js
+++ admin/client/js/slatwalladmin.js
@@ -1,7 +1,7 @@
 angular.module('slatwalladmin', ['slatwalladmin.services',function($locationProvider){
 	$locationProvider.html5Mode(true);
 }]).config(["$provide", function ($provide) {
-	$provide.constant("partialsPath", '/admin/client/slatwalladmin/js/directives/partials/');
+	$provide.constant("partialsPath", '/admin/client/js/directives/partials/');
 }]).run(function($rootScope) {
 	//adding alerts to the root Scope
     $rootScope.alerts = [];
