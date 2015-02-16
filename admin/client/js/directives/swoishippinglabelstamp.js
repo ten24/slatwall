a2e3bca25dbc71f712c5c649c652f6049136f733
Ryan Marchand <ryan.marchand@ten24web.com>
2015-02-16 16:27:36 -0500

    refactor of shipping,
    adding links for buttons

--- admin/client/js/directives/swoishippinglabelstamp.js
+++ admin/client/js/directives/swoishippinglabelstamp.js
@@ -3,51 +3,20 @@
  * @module slatwalladmin
  * @class swOrderItemsShippingLabelStamp
  */
-'use strict';
 angular.module('slatwalladmin')
 .directive('swoishippinglabelstamp', 
 [
-	function(){
-		var getTemplate = function(orderItem){
-			//Need to include other fulfillment methods.
-			//console.log("Setting Fulfillment Method");
-			//This will need to check for the type of fulfillment and not just assume a shipping address (IE returns)
-			//console.log(orderItem);
-			var addressStamp = "";
-			//console.log("_______Shipping method:");
-			//console.log(orderItem.data.orderFulfillment.data.fulfillmentMethodType);
-			if (orderItem.data.orderFulfillment.data.fulfillmentMethodType === "shipping"){
-				//console.log("_______\n\n\n");
-				//Set defaults.
-				var sa1 = orderItem.data.orderFulfillment.data.shippingAddress.data.streetAddress || " ";
-				var sa2 = orderItem.data.orderFulfillment.data.shippingAddress.data.street2Address || " ";
-				var city = orderItem.data.orderFulfillment.data.shippingAddress.data.city || " ";
-				var state = orderItem.data.orderFulfillment.data.shippingAddress.data.stateCode || " ";
-				var postal = orderItem.data.orderFulfillment.data.shippingAddress.data.postalCode || " ";
-				//console.log(city);
-				//Build the stamp.
-				addressStamp = "<ul><li>" + sa1 + "</li>" +
-				"<li>" + sa2 + "</li>" +
-				"<li>" + city + "&nbsp" 
-						  + state + "&nbsp<br>" 
-						  + postal + "</li></ul>";
-				
-			}else{
-				addressStamp = "<ul><li>N/A</li></ul>";
-			}
-			
-			return addressStamp; 
-		}
+'partialsPath',
+	function(partialsPath){
 		return {
 			restrict: 'E',
-			transclude:true,
 			scope:{
-				orderItem:"="
+				orderFulfillment:"="
 			},
-			replace:true,
+			templateUrl:partialsPath+"orderfulfillment-shippinglabel.html",
 			link: function(scope, element, attrs){
 				//Get the template.
-				element.html(getTemplate(scope.orderItem));
+				
 			}
 		};
 	}
