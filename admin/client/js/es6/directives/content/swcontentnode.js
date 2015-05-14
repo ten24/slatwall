aefa676dbb3d511f41cabe80654aac4fdc6269fa
Ryan Marchand <ryan.marchand@ten24web.com>
2015-05-14 13:08:55 -0400

    updates to list

--- admin/client/js/es6/directives/content/swcontentnode.js
+++ admin/client/js/es6/directives/content/swcontentnode.js
@@ -12,16 +12,11 @@ angular.module('slatwalladmin').directive('swContentNode', [
             },
             templateUrl: partialsPath + 'content/contentnode.html',
             link: function (scope, element, attr) {
-                if (angular.isDefined(scope.$parent)) {
-                    if (angular.isDefined(scope.$parent.child)) {
-                        scope.contentData = scope.$parent.child;
-                        if (angular.isUndefined(scope.depth) && angular.isUndefined(scope.$parent.depth)) {
-                            scope.depth = 1;
-                        }
-                        else {
-                            scope.depth = scope.$parent.depth + 1;
-                        }
-                    }
+                if (angular.isUndefined(scope.depth)) {
+                    scope.depth = 0;
+                }
+                if (angular.isDefined(scope.$parent.depth)) {
+                    scope.depth = scope.$parent.depth + 1;
                 }
                 var childContentColumnsConfig = [{
                     propertyIdentifier: '_content.contentID',
@@ -54,6 +49,7 @@ angular.module('slatwalladmin').directive('swContentNode', [
                     isSearchable: true
                 }];
                 scope.getChildContent = function (parentContentRecord) {
+                    scope.childOpen = true;
                     var childContentfilterGroupsConfig = [{
                         "filterGroup": [{
                             "propertyIdentifier": "_content.parentContent.contentID",
@@ -68,12 +64,11 @@ angular.module('slatwalladmin').directive('swContentNode', [
                     });
                     collectionListingPromise.then(function (value) {
                         parentContentRecord.children = value.records;
-                        var table = document.getElementById("contentTable");
+                        var index = 0;
                         angular.forEach(parentContentRecord.children, function (child) {
-                            scope.child = child;
-                            element.after($compile('<tr class="childNode" style="margin-left:15px" sw-content-node data-content-data="child"></tr>')(scope));
-                            //element.parent().children().splice(element.index,0,)(scope));
-                            //element.parent().append($compile('<tr class="childNode" style="margin-left:15px" sw-content-node data-content-data="child"></tr>')(scope));
+                            scope['child' + index] = child;
+                            element.after($compile('<tr class="childNode" style="margin-left:{{depth*15||0}}px"  sw-content-node data-content-data="child' + index + '"></tr>')(scope));
+                            index++;
                         });
                     });
                 };
