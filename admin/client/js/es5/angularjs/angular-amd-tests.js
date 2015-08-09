a3f14fa806479dede13e53f6ba041f5b3dafd96d
Ryan Marchand <ryan.marchand@ten24web.com>
2015-08-09 16:20:41 -0400

    updating definitions

---
+++ admin/client/js/es5/angularjs/angular-amd-tests.js
@@ -0,0 +1,7 @@
+/// <reference path="angular.d.ts" />
+var localName = require('angular');
+var mod = localName.module('mod', []);
+// Remain compatible with the ambient version 
+var mod2 = mod;
+
+//# sourceMappingURL=../angularjs/angular-amd-tests.js.map
