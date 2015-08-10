fc313fbf4781d982ef63521e4293d73c0565a5e4
jasperboyd <jasper.boyd@ten24web.com>
2015-08-10 16:44:08 -0400

    unfinished exception handler typescript

--- admin/client/ts/services/exceptionhandler.ts
+++ admin/client/ts/services/exceptionhandler.ts
@@ -1,27 +1,45 @@
 /// <reference path="../../../../client/typings/tsd.d.ts" />
 /// <reference path="../../../../client/typings/slatwallTypeScript.d.ts" />
 
+import slatwalladmin = require("slatwalladmin");
+import {auto as ngAuto} from "angular";
+
 module logger{
-	angular.module('logger')
-	.factory('$exceptionHandler', [ '$injector', function ($injector) {
-	    return function (exception, cause) {
-	    	var $http = $injector.get('$http');
-            var alertService = $injector.get('alertService');
-	        $http({
-	        	url:'?slatAction=api:main.log',
-	        	method:'POST', 
-	        	data:$.param({
-                    exception:exception,
-                    cause:cause,
-                    apiRequest:true
-                }),
-	        	headers:{'Content-Type': 'application/x-www-form-urlencoded'}
-	        }).error(function(data){
-                console.log(exception);
-                alertService.addAlert({msg:exception,type:'error'});
-                console.log(exception);
-            });
-            
-	    }
-	}]);
+
+	export interface IExceptionThrown {
+		paramException: error, 
+		cause: string
+	}; 
+
+	export class ClientLoggingService implements ng.IExceptionHandlerService {
+
+		static $inject = ["$injector"];
+
+		constructor($injector: ngAuto.IInjectorService) {
+            this.$injector = $injector; 
+        }
+
+   		public exception: ng.IExceptionHandlerService = (paramException: Error, cause?: string):func => {
+			return function():IExceptionThrown{ 
+				var $http = this.$injector.get('$http');
+	            var alertService = this.$injector.get('alertService');
+		      	
+		      	$http({
+		        	url:'?slatAction=api:main.log',
+		        	method:'POST', 
+		        	data:$.param({
+	                    exception:paramException,
+	                    cause:cause,
+	                    apiRequest:true
+	                }),
+		        	headers:{'Content-Type': 'application/x-www-form-urlencoded'}
+		        }).error(function(data){
+	                alertService.addAlert({msg:paramException,type:'error'});
+	           	    console.log(paramException);
+	            });
+	    	};	
+    	};
+    	
+	}
+	angular.module('logger').factory('$exceptionHandler', ["$injector", ($injector) => new ClientLoggingService($injector)]);
 }
