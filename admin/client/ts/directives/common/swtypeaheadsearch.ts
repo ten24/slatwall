2c70289dea754361cd3ca9aa1a19d08bb4c8d34f
jasperboyd <jasper.boyd@ten24web.com>
2015-10-06 10:45:03 -0400

    building out logic

--- admin/client/ts/directives/common/swtypeaheadsearch.ts
+++ admin/client/ts/directives/common/swtypeaheadsearch.ts
@@ -6,10 +6,34 @@ module slatwalladmin {
 		public static $inject=["$slatwall", "collectionConfigService"];
 		public entity:string;
 		public properties:string;
+		public allRecords:boolean; 
 		public placeholderText:string;  
+		public searchText:string; 
+		public results; 
+		private typeaheadCollectionConfig; 
 		
 		constructor(private $slatwall:ngSlatwall.$Slatwall, private collectionConfigService:slatwalladmin.collectionConfigService){
-			collectionConfigService.newCollectionConfig(this.entity, this.properties); 
+			this.typeaheadCollectionConfig = collectionConfigService.newCollectionConfig(this.entity, this.properties);
+			
+			if(angular.isDefined(this.allRecords)){
+				this.typeaheadCollectionConfig.setAllRecords(this.allRecords)
+			} else {
+				this.typeaheadCollectionConfig.setAllRecords(true); 
+			}
+		}
+		
+		public search = (search:string)=>{
+			
+			this.typeaheadCollectionConfig.setKeywords(search); 
+			var promise = this.typeaheadCollectionConfig.getEntity();
+			
+			promise.then((response)=>{
+				if(angular.isDefined(this.allRecords) && this.allRecords == false){
+					this.results = response.pageRecords;
+				} else {
+					this.results = response;
+				}	 
+			});
 		}
 		
 	}
@@ -24,7 +48,9 @@ module slatwalladmin {
 		public bindToController = {
 			entity:"=",
 			properties:"=",
-			placeholderText:"=?"
+			placeholderText:"=?",
+			searchText:"=?",
+			results:"=?"
 		}
 		public controller=SWTypeaheadSearchController;
         public controllerAs="swTypeaheadSearch";
@@ -40,6 +66,9 @@ module slatwalladmin {
 		}
     }
     
-    angular.module('slatwalladmin').directive('swTypeaheadSearch',[() => new SWTypeaheadSearch()]); 
+    angular.module('slatwalladmin').directive('swTypeaheadSearch',
+		["$slatwall", "collectionConfigService", "partialsPath", 
+			($slatwall, collectionConfigService, partialsPath) => 
+				new SWTypeaheadSearch($slatwall, collectionConfigService, partialsPath)]); 
 
 }
