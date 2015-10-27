fb37ffe8e4c059f00d56a0d2ffda8548be92465c
ian-a-hickey <ian.hickey@ten24web.com>
2015-10-27 12:36:10 -0400

    refactored account cart and processObject to Typescript

---
+++ custom/apps/frontendcode/frontendtesting/SlatwallFrontendLibraryTS/swf-services/processObjectService.ts
@@ -0,0 +1,34 @@
+/// <reference path='../../../../../../client/typings/slatwallTypescript.d.ts' />
+/// <reference path='../../../../../../client/typings/tsd.d.ts' />
+module slatwallFrontend {
+    export interface IPostFactory {
+            dataFactory: {},
+            Get: () => {};
+    }
+    export class CartFactory implements IPostFactory {
+        /**
+         * DataFactory contains all endpoints available to the Account Object.
+         */
+        dataFactory ={
+            $get(processObject, entityName) {
+                var urlBase = "/index.cfm/api/scope/getProcessObjectDefinition/?ajaxRequest=1&processObject="+processObject+"&entityName="+entityName+"";
+                return this.http.get(urlBase);
+            }
+        };
+        
+        public http: ng.IHttpService;
+        static $inject = ['$http'];
+        
+        constructor($http: ng.IHttpService) {
+            this.http = $http;
+        }
+
+        /**
+         * Returns an instance of the dataFactory
+         */
+        Get(): {} {
+            return this.dataFactory;
+        }
+    }
+}
+
