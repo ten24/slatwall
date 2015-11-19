83b46617d3eed9005795229946e457681d09b82c
Ryan Marchand <ryan.marchand@ten24web.com>
2015-11-19 11:37:13 -0500

    adding product module

--- org/Hibachi/client/src/hibachi/product/controllers/preprocessproduct_create.ts
+++ org/Hibachi/client/src/hibachi/product/controllers/preprocessproduct_create.ts
@@ -1,28 +1,26 @@
-module slatwalladmin {
-        'use strict';
-	
-	
-	export class ProductCreateController{
-                
-                
+
+
+class ProductCreateController{
+        
+        
         public static $inject=["$scope",'$element','$log', "$slatwall","collectionConfigService","selectionService"];        
         
-		constructor(
-            private $scope: IOrderItemGiftRecipientScope, 
-            private $element, private $log:ng.ILogService,  
-            private $slatwall:ngSlatwall.$Slatwall, 
-            private collectionConfigService,
-            private selectionService
+                constructor(
+                private $scope: IOrderItemGiftRecipientScope, 
+                private $element, private $log:ng.ILogService,  
+                private $slatwall:ngSlatwall.$Slatwall, 
+                private collectionConfigService,
+                private selectionService
         ){
-            this.$log.debug('init product_create controller');
-            //on select change get collection
-            this.$scope.preprocessproduct_createCtrl.productTypeChanged = (selectedOption)=>{
+                this.$log.debug('init product_create controller');
+                //on select change get collection
+                this.$scope.preprocessproduct_createCtrl.productTypeChanged = (selectedOption)=>{
                 this.$scope.preprocessproduct_createCtrl.selectedOption = selectedOption;
                 this.$scope.preprocessproduct_createCtrl.getCollection();
                 this.selectionService.clearSelection('ListingDisplay');
-            }  
-            
-            this.$scope.preprocessproduct_createCtrl.getCollection = ()=>{
+                }  
+                
+                this.$scope.preprocessproduct_createCtrl.getCollection = ()=>{
                 this.collectionConfig = this.collectionConfigService.newCollectionConfig('Option');
                 this.collectionConfig.setDisplayProperties('optionGroup.optionGroupName,optionName',undefined,{isVisible:true});
                 this.collectionConfig.addDisplayProperty('optionID',undefined,{isVisible:false});
@@ -32,25 +30,22 @@ module slatwalladmin {
                 this.collectionConfig.setOrderBy('optionGroup.sortOrder|ASC,sortOrder|ASC');
                 this.$scope.preprocessproduct_createCtrl.collectionListingPromise = this.collectionConfig.getEntity();
                 this.$scope.preprocessproduct_createCtrl.collectionListingPromise.then((data)=>{
-                    this.$scope.preprocessproduct_createCtrl.collection = data;    
-                    this.$scope.preprocessproduct_createCtrl.collection.collectionConfig = this.collectionConfig;
+                        this.$scope.preprocessproduct_createCtrl.collection = data;    
+                        this.$scope.preprocessproduct_createCtrl.collection.collectionConfig = this.collectionConfig;
                 })
-            }
-            
-            var jQueryOptions = $("select[name='product.productType.productTypeID']")[0];
-            this.$scope.preprocessproduct_createCtrl.options = [];
-            angular.forEach(jQueryOptions,(jQueryOption)=>{
+                }
+                
+                var jQueryOptions = $("select[name='product.productType.productTypeID']")[0];
+                this.$scope.preprocessproduct_createCtrl.options = [];
+                angular.forEach(jQueryOptions,(jQueryOption)=>{
                 var option = {
-                    label:jQueryOption.label,
-                    value:jQueryOption.value    
+                        label:jQueryOption.label,
+                        value:jQueryOption.value    
                 }
                 this.$scope.preprocessproduct_createCtrl.options.push(option); 
-            });
-            this.$scope.preprocessproduct_createCtrl.productTypeChanged(this.$scope.preprocessproduct_createCtrl.options[0]);
-		}
+                });
+                this.$scope.preprocessproduct_createCtrl.productTypeChanged(this.$scope.preprocessproduct_createCtrl.options[0]);
+                }
         
-	}
-	
-	angular.module('slatwalladmin').controller('preprocessproduct_create', ProductCreateController);
-
-}
+        }
+}	
