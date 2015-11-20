8190e6e10629a502d4378b35c456deaec9beb450
jasperboyd <jasper.boyd@ten24web.com>
2015-11-20 08:59:11 -0500

    gift card module

--- org/Hibachi/client/src/hibachi/hibachi.module.ts
+++ org/Hibachi/client/src/hibachi/hibachi.module.ts
@@ -5,12 +5,14 @@ import {alertmodule} from "./alert/alert.module";
 import {coremodule} from "./core/core.module";
 import {paginationmodule} from "./pagination/pagination.module";
 import {dialogmodule} from "./dialog/dialog.module";
+import {giftcardmodule} from "./giftcard/giftcard.module";
 import {collectionmodule} from "./collection/collection.module";
 import {workflowmodule} from "./workflow/workflow.module";
 
 var hibachimodule = angular.module('hibachi',[
     alertmodule.name,
     coremodule.name,
+    giftcardmodule.name,
     paginationmodule.name,
     dialogmodule.name,
     collectionmodule.name,
