3c4e86b81203fcc387018ff52588cf75f65b81f1
ian-a-hickey <ian.hickey@ten24web.com>
2015-11-23 16:05:29 -0500

    formfields refactored to ts

--- admin/client/ts/directives/common/form/swformfield.ts
+++ admin/client/ts/directives/common/form/swformfield.ts
@@ -1,38 +1,39 @@
-angular.module('slatwalladmin')
-.directive('swFormField', [
-'$log',
-'$templateCache',
-'$window',
-'$slatwall',
-'formService',
-'partialsPath',
-	function(
-	$log,
-	$templateCache,
-	$window,
-	$slatwall,
-	formService,
-	partialsPath
-	){
-		return {
-			require:"^form",
-			restrict: 'AE',
-			scope:{
-				propertyDisplay:"="
-			},
-			templateUrl:partialsPath+'formfields/formfield.html',
-			link: function(scope, element,attrs,formController){
-				if(angular.isUndefined(scope.propertyDisplay.object.$$getID) || scope.propertyDisplay.object.$$getID() === ''){
-					scope.propertyDisplay.isDirty = true;
-				} 
-				 
-				if(angular.isDefined(formController[scope.propertyDisplay.property])){
-					scope.propertyDisplay.errors = formController[scope.propertyDisplay.property].$error;
-					formController[scope.propertyDisplay.property].formType = scope.propertyDisplay.fieldType;
+/// <reference path='../../../../../../client/typings/slatwallTypescript.d.ts' />
+/// <reference path='../../../../../../client/typings/tsd.d.ts' />
+module slatwalladmin {
+
+    export class swFormField implements ng.IDirective {
+        public static $inject = ['$log','templateCache','$window','$slatwall','formService','partialsPath'];
+		constructor(public $log, public $templateCache, public $window, public $slatwall, public formService, public partialsPath){
+			this.partialsPath = partialsPath;
+			this.$templateCache = $templateCache;
+			this.$window = $window;
+			this.$slatwall = $slatwall;
+			this.formService = formService;
+			this.partialsPath = partialsPath;
+            return this.GetInstance();
+        }
+
+        GetInstance(): any {
+            return {
+                require:"^form",
+				restrict: 'AE',
+				scope:{
+					propertyDisplay:"="
+				},
+				templateUrl:this.partialsPath+'formfields/formfield.html',
+				link: (scope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, formController: ng.IFormController) =>{
+					if(angular.isUndefined(scope.propertyDisplay.object.$$getID) || scope.propertyDisplay.object.$$getID() === ''){
+						scope.propertyDisplay.isDirty = true;
+					} 
+					
+					if(angular.isDefined(formController[scope.propertyDisplay.property])){
+						scope.propertyDisplay.errors = formController[scope.propertyDisplay.property].$error;
+						formController[scope.propertyDisplay.property].formType = scope.propertyDisplay.fieldType;
+					}
 				}
-				
-			}
-		};
+            };
+        }
 	}
-]);
-	
+	angular.module('slatwalladmin').directive('swFormField',['$log','$templateCache', '$window', '$slatwall', 'formService', 'partialsPath',($log, $templateCache, $window, $slatwall, formService, partialsPath) => new swFormField($log, $templateCache, $window, $slatwall, formService, partialsPath)]);
+}
--- admin/client/ts/directives/common/form/swformfieldnumber.ts
+++ admin/client/ts/directives/common/form/swformfieldnumber.ts
@@ -1,26 +1,32 @@
-angular.module('slatwalladmin')
-.directive('swFormFieldNumber', [
-'$log',
-'$slatwall',
-'formService',
-'partialsPath',
-	function(
-	$log,
-	$slatwall,
-	formService,
-	partialsPath
-	){
-		return{
-			templateUrl:partialsPath+'formfields/number.html',
-			require:"^form",
-			restrict: 'E',
-			scope:{
-				propertyDisplay:"="
-			},
-			link:function(scope,element,attr,formController){
-				scope.propertyDisplay.form[scope.propertyDisplay.property].$dirty = scope.propertyDisplay.isDirty;
-			}
-		};
+/// <reference path='../../../../../../client/typings/slatwallTypescript.d.ts' />
+/// <reference path='../../../../../../client/typings/tsd.d.ts' />
+
+module slatwalladmin {
+export class SWFormFieldNumberController implements ng.IDirective {
+	constructor () {
+        if (this.propertyDisplay.isDirty == undefined) this.propertyDisplay.isDirty = false;
+		this.propertyDisplay.form.$dirty = this.propertyDisplay.isDirty;
 	}
-]);
+}	
+	
+export class SWFormFieldNumber implements ng.IDirective {
+        public restrict = 'E';
+        public require = "^form";
+        public scope = true;
+        public bindToController = {
+            propertyDisplay: "=?"
+        };
+        public templateUrl = "";
+		public controller = SWFormFieldNumberController;
+        public controllerAs = "ctrl";
+        
+        public link = (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, formController: ng.IFormController) => {}
+        
+        public static $inject = ['$log', '$slatwall', 'formService', 'partialsPath'];
+		constructor(public $log, $slatwall, formService, public partialsPath) {
+            this.templateUrl = this.partialsPath + "formfields/number.html";
+        }
+    }
+    angular.module('slatwalladmin').directive('swFormFieldNumber', ['$log','$slatwall','formService','partialsPath', ($log, $slatwall, formService, partialsPath) => new SWFormFieldNumber($log, $slatwall, formService, partialsPath)]);
+}
 	
