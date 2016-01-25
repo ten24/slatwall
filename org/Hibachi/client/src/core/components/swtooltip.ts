5cb9a38851dd43ba09a4f3d087acf6aed35a6b2c
jasperboyd <jasper.boyd@ten24web.com>
2016-01-25 15:42:01 -0500

    initial implementation

--- org/Hibachi/client/src/core/components/swtooltip.ts
+++ org/Hibachi/client/src/core/components/swtooltip.ts
@@ -2,37 +2,50 @@
 /// <reference path='../../../typings/tsd.d.ts' />
 class SWTooltipController {
 
+    public rbKey; 
+    public text; 
+    public position; 
+    public showTooltip:boolean = false; 
+
     // @ngInject
-	constructor(){
-        
+	constructor(public rbkeyService){
+        this.text = rbkeyService.getRBKey(this.rbKey);
 	}
 }
 
 class SWTooltip implements ng.IDirective{
 
-	public static $inject=["$hibachi", "$timeout", "collectionConfigService", "corePartialsPath",
-			'hibachiPathBuilder'];
 	public templateUrl;
     public transclude=false;
 	public restrict = "E";
 	public scope = {}
 
 	public bindToController = {
-        
+        rbKey:"@",
+        position:"@",
+        showTooltip:"=?"
 	}
 	public controller=SWTooltipController;
 	public controllerAs="swTooltip";
 
-	constructor(private corePartialsPath,hibachiPathBuilder){
+    // @ngInject
+	constructor( public $document, private corePartialsPath, hibachiPathBuilder){
 	   this.templateUrl = hibachiPathBuilder.buildPartialsPath(corePartialsPath) + "tooltip.html";
     }
 
 	public link:ng.IDirectiveLinkFn = (scope:any, element:any, attrs:any, controller:any, transclude:any) =>{
-	}
+	   this.$document.on("mouseenter", ()=>{
+           scope.swTooltip.showTooltip = true;
+       });
+       
+       this.$document.exit("mouseenter", ()=>{
+           scope.swTooltip.showTooltip = false;
+       });
+    }
     
 	public static Factory(){
-		var directive:ng.IDirectiveFactory = (corePartialsPath,hibachiPathBuilder) => new SWTooltip(corePartialsPath,hibachiPathBuilder);
-		directive.$inject = ["corePartialsPath","hibachiPathBuilder"];
+		var directive:ng.IDirectiveFactory = ($document,corePartialsPath,hibachiPathBuilder) => new SWTooltip($document,corePartialsPath,hibachiPathBuilder);
+		directive.$inject = ["$document","corePartialsPath","hibachiPathBuilder"];
 		return directive;
 	}
 }
