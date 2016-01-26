764c8d002ec74b19bb2503e861dd43a5ce389a60
jasperboyd <jasper.boyd@ten24web.com>
2016-01-26 16:40:34 -0500

    fixing

--- admin/client/src/orderitem/components/sworderitem.ts
+++ admin/client/src/orderitem/components/sworderitem.ts
@@ -223,7 +223,7 @@ class SWOrderItem{
 						"isDeletable":true
 						},
 						{
-							"propertyIdentifier":"_orderitem.sku.price",
+							"propertyIdentifier":"_orderitem.skuPrice",
 							"ormtype":"string"
 						},
 						{
