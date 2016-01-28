b1caf3e25a539a91d656fc3bab00913a708dd5e2
Ryan Marchand <ryan.marchand@ten24web.com>
2016-01-28 14:55:46 -0500

    pushing filters to core

--- org/Hibachi/client/src/core/core.module.ts
+++ org/Hibachi/client/src/core/core.module.ts
@@ -75,6 +75,31 @@ var coremodule = angular.module('hibachi.core',[
              }
          };
      });
+
+     $filterProvider.register('truncate',function(){
+         return function (input, chars, breakOnWord) {
+             if (isNaN(chars)) return input;
+             if (chars <= 0) return '';
+             if (input && input.length > chars) {
+                 input = input.substring(0, chars);
+                 if (!breakOnWord) {
+                     var lastspace = input.lastIndexOf(' ');
+                     //get last space
+                     if (lastspace !== -1) {
+                         input = input.substr(0, lastspace);
+                     }
+                 }else{
+                     while(input.charAt(input.length-1) === ' '){
+                         input = input.substr(0, input.length -1);
+                     }
+                 }
+                 return input + '...';
+             }
+             return input;
+         };
+     });
+    
+    
     hibachiPathBuilder.setBaseURL(appConfig.baseURL);
     hibachiPathBuilder.setBasePartialsPath('/org/Hibachi/client/src/');
     $provide.decorator('$hibachi',[
