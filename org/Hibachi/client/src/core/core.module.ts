cb2e1bc94710ea8645d131f8f6be2ecd4331052e
Miguel Targa <miguel@games.com>
2016-03-01 13:09:20 -0500

    ability to have filtergroups: ``` <sw-listing-filter-group> 	<sw-listing-filter {...}></sw-listing-filter> 	<sw-listing-filter {...}></sw-listing-filter> </sw-listing-filter-group>
    <sw-listing-filter {...}></sw-listing-filter>
    ```
    
    Radio button in listing display fixed

--- org/Hibachi/client/src/core/core.module.ts
+++ org/Hibachi/client/src/core/core.module.ts
@@ -40,6 +40,7 @@ import {SWListingAggregate} from "./components/swlistingaggregate";
 import {SWListingColorFilter} from "./components/swlistingcolorfilter";
 import {SWListingColumn} from "./components/swlistingcolumn";
 import {SWListingFilter} from "./components/swlistingfilter";
+import {SWListingFilterGroup} from "./components/swlistingfilterGroup";
 import {SWListingOrderBy} from "./components/swlistingorderby";
 import {SWLogin} from "./components/swlogin";
 import {SWNumbersOnly} from "./components/swnumbersonly";
@@ -1232,6 +1233,7 @@ var coremodule = angular.module('hibachi.core',[
 .directive('swListingColorFilter',SWListingColorFilter.Factory())
 .directive('swListingColumn',SWListingColumn.Factory())
 .directive('swListingFilter',SWListingFilter.Factory())
+.directive('swListingFilterGroup',SWListingFilterGroup.Factory())
 .directive('swListingOrderBy',SWListingOrderBy.Factory())
 .directive('swLogin',SWLogin.Factory())
 .directive('swNumbersOnly',SWNumbersOnly.Factory())
