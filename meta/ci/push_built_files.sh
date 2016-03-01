24007a2a2be3fc0819b5cc2814bd9feed4c1e723
Greg Moser <greg@gregmoser.com>
2016-02-29 16:34:21 -0800

    Turned script back on

--- meta/ci/push_built_files.sh
+++ meta/ci/push_built_files.sh
@@ -2,12 +2,13 @@
 
 if git diff-index --quiet HEAD --; then
     # no changes
-    #echo No Changes To Push
-    echo no changes
+    echo No Changes To Push
+
 else
 
     # changes
-    #git commit -a -m "CI build passed, commit of automatically built files - $CIRCLE_BUILD_URL [skip ci]"
-    #git push origin current
-    echo changes
+    echo Build Changes Found
+    git commit -a -m "CI build passed, commit of automatically built files - $CIRCLE_BUILD_URL [skip ci]"
+    git push origin current
+
 fi
