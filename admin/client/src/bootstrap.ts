7f199b738c79bdf169472762dd051eefed35f901
Ryan Marchand <ryan.marchand@ten24web.com>
2016-03-03 13:33:35 -0500

    fixing typescript

--- admin/client/src/bootstrap.ts
+++ admin/client/src/bootstrap.ts
@@ -8,7 +8,7 @@ import {slatwalladminmodule} from "./slatwall/slatwalladmin.module";
 class bootstrapper extends BaseBootStrapper{
     public myApplication;
     constructor(){
-        var angular = super(slatwalladminmodule.name);
+        var angular:any = super(slatwalladminmodule.name);
         angular.bootstrap()
     }
 
