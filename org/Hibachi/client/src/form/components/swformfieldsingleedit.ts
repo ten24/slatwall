e6380cb8558c71d3164fdd0b9f36a5e0dfa0b33c
jasperboyd <jasper.boyd@ten24web.com>
2016-05-04 10:58:31 -0400

    adding in boilerplate for SWFormFieldSingleEdit

---
+++ org/Hibachi/client/src/form/components/swformfieldsingleedit.ts
@@ -0,0 +1,96 @@
+/// <reference path='../../../typings/hibachiTypescript.d.ts' />
+/// <reference path='../../../typings/tsd.d.ts' />
+class SWFormFieldSingleEditController {
+    
+    public property:string;
+    public object:any;
+    public options:{};
+    public editable:boolean;
+    public editing:boolean;
+    public isHidden:boolean;
+    public title:string;
+    public hint:string;
+    public optionsArguments:string;
+    public eagerLoadOptions;
+    public isDirty:boolean;
+    public onChange;
+    public fieldType:string;
+    public noValidate:boolean;
+
+    // @ngInject
+    constructor(private $hibachi
+        ){
+        
+    }
+    
+    public save = () => {
+        
+    }
+
+}
+
+class SWFormFieldSingleEdit implements ng.IDirective{
+
+    public templateUrl;
+    public restrict = "EA";
+    public scope = {};
+
+    public bindToController = {
+        property:"@",
+        object:"=?",
+        options:"=?",
+        editable:"=?",
+        editing:"=?",
+        isHidden:"=?",
+        title:"=?",
+        hint:"@?",
+        optionsArguments:"=?",
+        eagerLoadOptions:"=?",
+        isDirty:"=?",
+        onChange:"=?",
+        fieldType:"@?",
+        noValidate:"=?"
+    };
+    public controller=SWFormFieldSingleEdit;
+    public controllerAs="swFormFieldSingleEdit";
+
+    // @ngInject
+    constructor(public $compile, private coreFormPartialsPath,hibachiPathBuilder){
+        this.templateUrl = hibachiPathBuilder.buildPartialsPath(coreFormPartialsPath) + "formfieldsingleedit.html";
+    }
+
+    public link:ng.IDirectiveLinkFn = ($scope, element: ng.IAugmentedJQuery, attrs:ng.IAttributes, modelCtrl: ng.INgModelController) =>{
+
+         modelCtrl.$parsers.unshift((inputValue) =>{
+            var modelValue = modelCtrl.$modelValue;
+            
+            //figure out if the model value has changed
+            
+            //update editable
+                
+            return modelValue;
+        });
+
+
+    }
+
+    public static Factory(){
+        var directive:ng.IDirectiveFactory = (
+            $compile
+            ,coreFormPartialsPath
+            ,hibachiPathBuilder
+
+        )=> new SWFormFieldSingleEdit(
+            $compile
+            ,coreFormPartialsPath
+            ,hibachiPathBuilder
+        );
+        directive.$inject = ["$compile","coreFormPartialsPath",
+            'hibachiPathBuilder'];
+        return directive;
+    }
+}
+export{
+    SWFormFieldSingleEdit,
+    SWFormFieldSingleEditController
+}
