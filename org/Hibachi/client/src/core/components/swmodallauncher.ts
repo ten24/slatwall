279eca3fb6077f875d815df708f2f8964291ff22
jasperboyd <jasper.boyd@ten24web.com>
2016-05-09 15:50:39 -0400

    stump file for modal launcher directive

---
+++ org/Hibachi/client/src/core/components/swmodallauncher.ts
@@ -0,0 +1,60 @@
+/// <reference path='../../../typings/hibachiTypescript.d.ts' />
+/// <reference path='../../../typings/tsd.d.ts' />
+class SWModalLauncherController {
+
+
+    // @ngInject
+    constructor(
+        ){
+
+    }
+
+}
+
+class SWModalLauncher implements ng.IDirective{
+
+    public templateUrl;
+    public transclude=true; 
+    public restrict = "EA";
+    public scope = {};
+
+    public bindToController = {
+
+    };
+    public controller=SWModalLauncherController;
+    public controllerAs="swModalLauncher";
+
+    // @ngInject
+    constructor(public $compile, private corePartialsPath,hibachiPathBuilder){
+        this.templateUrl = hibachiPathBuilder.buildPartialsPath(corePartialsPath) + "modallauncher.html";
+    }
+
+    public compile = (element: JQuery, attrs: angular.IAttributes, transclude: any) => {
+        return {
+            pre: ($scope: any, element: JQuery, attrs: angular.IAttributes) => {      
+            },
+            post: ($scope: any, element: JQuery, attrs: angular.IAttributes) => {
+            }
+        };
+    }
+
+    public static Factory(){
+        var directive:ng.IDirectiveFactory = (
+            $compile
+            ,corePartialsPath
+            ,hibachiPathBuilder
+
+        )=> new SWModalLauncher(
+            $compile
+            ,corePartialsPath
+            ,hibachiPathBuilder
+        );
+        directive.$inject = ["$compile","corePartialsPath",
+            'hibachiPathBuilder'];
+        return directive;
+    }
+}
+export{
+    SWModalLauncher,
+    SWModalLauncherController
+}
--- org/Hibachi/client/src/core/core.module.ts
+++ org/Hibachi/client/src/core/core.module.ts
@@ -49,6 +49,7 @@ import {SWListingFilter} from "./components/swlistingfilter";
 import {SWListingFilterGroup} from "./components/swlistingfiltergroup";
 import {SWListingOrderBy} from "./components/swlistingorderby";
 import {SWLogin} from "./components/swlogin";
+import {SWModalLauncher} from "./components/swModalLauncher";
 import {SWNumbersOnly} from "./components/swnumbersonly";
 import {SWLoading} from "./components/swloading";
 import {SWScrollTrigger} from "./components/swscrolltrigger";
@@ -176,6 +177,7 @@ var coremodule = angular.module('hibachi.core',[
 .directive('swListingFilterGroup',SWListingFilterGroup.Factory())
 .directive('swListingOrderBy',SWListingOrderBy.Factory())
 .directive('swLogin',SWLogin.Factory())
+.directive('swModalLauncher',SWModalLauncher.Factory())
 .directive('swNumbersOnly',SWNumbersOnly.Factory())
 .directive('swLoading',SWLoading.Factory())
 .directive('swScrollTrigger',SWScrollTrigger.Factory())
