099e22c3753c3e6df69e3cef973be742cfbf06f7
jasperboyd <jasper.boyd@ten24web.com>
2016-05-27 10:59:07 -0400

    adding boilerplate for a listing service

---
+++ org/Hibachi/client/src/core/services/listingservice.ts
@@ -0,0 +1,10 @@
+/// <reference path='../../../typings/hibachiTypescript.d.ts' />
+/// <reference path='../../../typings/tsd.d.ts' />
+class ListingService{
+    constructor(){
+    }
+    
+    
+}
+export{ListingService};
+
