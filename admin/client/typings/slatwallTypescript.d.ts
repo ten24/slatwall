c3dd1db11e8922257c9bcc3e9800e010f94fe20b
Big Boy (Slatwall Robot) <info@slatwallcommerce.com>
2016-07-15 21:10:18 +0000

    CI build passed, auto-built files commit - https://circleci.com/gh/ten24/slatwall/2326 [ci skip]

--- admin/client/typings/slatwallTypescript.d.ts
+++ admin/client/typings/slatwallTypescript.d.ts
@@ -39,7 +39,7 @@
 /// <reference path="../src/productbundle/components/swproductbundlegrouptype.ts" />
 /// <reference path="../src/productbundle/controllers/create-bundle-controller.ts" />
 /// <reference path="../src/productbundle/services/productbundleservice.ts" />
-/// <reference path="../src/slatwall/filters/swcurrency.ts" />
 /// <reference path="../src/slatwall/components/swcurrencyformatter.ts" />
+/// <reference path="../src/slatwall/filters/swcurrency.ts" />
 /// <reference path="../src/slatwall/services/slatwallpathbuilder.ts" />
 //}
