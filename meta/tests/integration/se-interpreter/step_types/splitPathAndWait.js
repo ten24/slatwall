f209c2e51c3b10a333d2e010ce0948375dea5e25
Alexis Espinoza <alexis.espinoza@ten24web.com>
2016-10-18 11:45:01 -0400

    bundle product assertions+comments and end-to-end test

--- meta/tests/integration/se-interpreter/step_types/splitPathAndWait.js
+++ meta/tests/integration/se-interpreter/step_types/splitPathAndWait.js
@@ -2,7 +2,6 @@ exports.run = function(tr, cb) {
    var tools = require('../functions');
    var component = tools.split_path_and_wait(tr.currentStep());
   //combine arrays at an index
-  console.log(component);
    tr.script.steps.splice.apply(tr.script.steps, [tr.stepIndex+1, 0].concat(component));
    cb({'success': true});
 }
