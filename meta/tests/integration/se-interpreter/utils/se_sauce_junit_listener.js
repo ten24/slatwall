8448951c6f97d2b6d4e2cf944bf67a9d001f329f
Alexis Espinoza <alexis.espinoza@ten24web.com>
2016-10-20 13:00:38 -0400

    Circle CI test run #14

--- meta/tests/integration/se-interpreter/utils/se_sauce_junit_listener.js
+++ meta/tests/integration/se-interpreter/utils/se_sauce_junit_listener.js
@@ -73,7 +73,6 @@ Listener.prototype.startTestRun = function(testRun, info) {
   	console.log('DB Snapshot Reset Complete');
 	if (this.originalListener) { this.originalListener.startTestRun(testRun, info); }
 	}
-  exec("ls -la", puts);
   console.log("mysql --user=root --password=CiPassword --host=slatwalldb -v -v -v slatwalldb < /home/ubuntu/slatwall/slatwall_test_starting_point_snapshot.sql");
   exec("mysql --user=root --password=CiPassword --host=slatwalldb -v -v -v slatwalldb < /home/ubuntu/slatwall/slatwall_test_starting_point_snapshot.sql && echo OK || echo Failed", puts);
   
