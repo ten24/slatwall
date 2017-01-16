2ffde0cb7ae97911edf27bff807c2f08c5360e12
Gus Erickson <gus.erickson@ten24web.com>
2017-01-16 16:00:00 -0500

    Pull from develop

--- org/Hibachi/client/src/frontend/frontend.module.ts
+++ org/Hibachi/client/src/frontend/frontend.module.ts
@@ -21,11 +21,17 @@ var frontendmodule = angular.module('frontend', [hibachimodule.name])
     }else{
         hibachiPathBuilder.setBasePartialsPath('custom/client/src/');
     }
+    /** Sets the custom public integration point */
+    if (hibachiConfig && hibachiConfig.apiSubsystemName){
+        hibachiPathBuilder.setApiSubsystemName(hibachiConfig.apiSubsystemName);
+    }
+
 }])
 
 .run(['$rootScope', '$hibachi','publicService','hibachiPathBuilder','entityService', function($rootScope, $hibachi, publicService,hibachiPathBuilder,entityService) {
 	$rootScope.slatwall = $rootScope.hibachiScope;
     $rootScope.slatwall.getProcessObject = entityService.newProcessObject;
+    $rootScope.slatwall.$hibachi.appConfig.apiSubsystemName = hibachiPathBuilder.apiSubsystemName;
 }])
 
 //controllers
