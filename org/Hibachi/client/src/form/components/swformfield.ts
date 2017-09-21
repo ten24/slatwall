ca4eb657b9f6d4e83f97f4da21a74bf8701a55eb
Jasper Boyd <jasper.boyd@ten24web.com>
2017-09-21 13:32:00 -0400

    mergin versions

--- org/Hibachi/client/src/form/components/swformfield.ts
+++ org/Hibachi/client/src/form/components/swformfield.ts
@@ -146,6 +146,7 @@ class SWFormFieldController {
 
 		if(angular.isUndefined(this.options)){
 			if(!this.optionsArguments || !this.optionsArguments.hasOwnProperty('propertyIdentifier')){
+				console.log("setting optionArguments", this.optionsArguments);
 				this.optionsArguments={
 					'propertyIdentifier':this.propertyIdentifier
 				};
