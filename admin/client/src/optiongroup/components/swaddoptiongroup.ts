616a7ae60579d81d4c1b939ef8065e69bdd32241
Ryan Marchand <ryan.marchand@ten24web.com>
2017-11-29 09:53:09 -0500

    bug fix for missing option groups

--- admin/client/src/optiongroup/components/swaddoptiongroup.ts
+++ admin/client/src/optiongroup/components/swaddoptiongroup.ts
@@ -7,109 +7,116 @@ class optionWithGroup {
         public optionGroupID:string,
         public match:boolean
     ){
-        
+
     }
-    
+
     public toString = () => {
         return this.optionID;
     }
 }
 
 class SWAddOptionGroupController {
-    
+
     public optionGroups;
-    public optionGroupIds; 
-    public productId; 
-    public product; 
-    public productTypeID; 
+    public optionGroupIds;
+    public productId;
+    public product;
+    public productTypeID;
     public productCollectionConfig;
     public skuCollectionConfig;
-    public skus; 
-    public usedOptions; 
-    public selection; 
+    public skus;
+    public usedOptions;
+    public selection;
     public selectedOptionList;
-    public showValidFlag; 
-    public showInvalidFlag; 
-    
+    public showValidFlag;
+    public showInvalidFlag;
+    public skusProcessing;
+
     // @ngInject
-    constructor(private $hibachi, private $timeout, 
-                private collectionConfigService, 
+    constructor(private $hibachi, private $timeout,
+                private collectionConfigService,
                 private observerService,
                 private utilityService
     ){
-        
+
         this.optionGroupIds = this.optionGroups.split(",");
-        this.optionGroupIds.sort(); 
-        
+        this.optionGroupIds.sort();
+
         this.selection = [];
-        
-        this.showValidFlag = false; 
+
+        this.showValidFlag = false;
         this.showInvalidFlag = false;
-        
+
         for(var i=0; i<this.optionGroupIds.length; i++){
-            this.selection.push(new optionWithGroup("", this.optionGroupIds[i], false)); 
-        } 
-        
-        
+            this.selection.push(new optionWithGroup("", this.optionGroupIds[i], false));
+        }
+
+
         this.productCollectionConfig = collectionConfigService.newCollectionConfig("Product");
         this.productCollectionConfig.addDisplayProperty("productID, productName, productType.productTypeID");
-        
+
         this.productCollectionConfig.getEntity(this.productId).then((response)=>{
-            
-            this.product = response; 				
+
+            this.product = response;
             this.productTypeID = response.productType_productTypeID;
-            
+
             this.skuCollectionConfig = collectionConfigService.newCollectionConfig("Sku");
-            this.skuCollectionConfig.addDisplayProperty("skuID, skuCode, product.productID"); 
+            this.skuCollectionConfig.addDisplayProperty("skuID, skuCode, product.productID");
             this.skuCollectionConfig.addFilter("product.productID", this.productId);
             this.skuCollectionConfig.setAllRecords(true);
-            
+
             this.usedOptions = [];
-            
+
             this.skuCollectionConfig.getEntity().then((response)=>{
-                this.skus = response.records; 	
+                this.skus = response.records;
+                this.skusProcessing = this.skus.length;
                 angular.forEach(this.skus, (sku)=>{
-                    
+
                     var optionCollectionConfig = collectionConfigService.newCollectionConfig("Option");
-                    
+
                     optionCollectionConfig.addDisplayProperty("optionID, optionName, optionCode, optionGroup.optionGroupID");
                     optionCollectionConfig.setAllRecords(true);
                     optionCollectionConfig.addFilter("skus.skuID", sku.skuID);
-                    
+
                     optionCollectionConfig.getEntity().then((response)=>{
-                        this.usedOptions.push(
-                            utilityService.arraySorter(response.records, ["optionGroup_optionGroupID"])
-                        );
+                        if(response.records.length){
+                            this.usedOptions.push(
+                                utilityService.arraySorter(response.records, ["optionGroup_optionGroupID"])
+                            );
+                        }
+                        this.skusProcessing--;
                     });
                 });
-            }); 
-        }); 
-        
-        this.observerService.attach(this.validateOptions, "validateOptions");			
+            });
+        });
+
+        this.observerService.attach(this.validateOptions, "validateOptions");
     }
-    
+
     public getOptionList = () => {
-        return this.utilityService.arrayToList(this.selection);  
+        return this.utilityService.arrayToList(this.selection);
     }
-    
+
     public validateOptions = (args:Array<any>) => {
 
-        this.addToSelection(args[0], args[1].optionGroupID); 		
-        
+        this.addToSelection(args[0], args[1].optionGroupID);
+
         if( this.hasCompleteSelection() ){
             if(this.validateSelection()){
                 this.selectedOptionList = this.getOptionList();
-                this.showValidFlag = true; 
-                this.showInvalidFlag = false; 
-            } else { 
-                this.showValidFlag = false; 
-                this.showInvalidFlag = true; 
+                this.showValidFlag = true;
+                this.showInvalidFlag = false;
+            } else {
+                this.showValidFlag = false;
+                this.showInvalidFlag = true;
             }
         }
     }
-    
+
     private validateSelection = () => {
-        var valid = true; 
+        var valid = true;
+        console.log('usedOptions',this.usedOptions);
+
         angular.forEach(this.usedOptions, (combination) => {
             if(valid){
                 var counter = 0;
@@ -117,85 +124,85 @@ class SWAddOptionGroupController {
                     if(this.selection[counter].optionGroupID === usedOption.optionGroup_optionGroupID
                         && this.selection[counter].optionID != usedOption.optionID
                     ){
-                        this.selection[counter].match = true; 
+                        this.selection[counter].match = true;
                     }
-                    counter++; 
+                    counter++;
                 });
                 if(!this.allSelectionFieldsValidForThisCombination()){
-                    valid = false; 
-                } 
+                    valid = false;
+                }
             }
-        }); 
-        
-        return valid; 
-    }	
-    
+        });
+
+        return valid;
+    }
+
     private allSelectionFieldsValidForThisCombination = () =>{
-        var matches = 0; 
+        var matches = 0;
         angular.forEach(this.selection, (pair)=>{
             if(!pair.match){
-                matches++; 
+                matches++;
             }
-            //reset 
-            pair.match = false; 
-        }); 
-        return matches != this.selection.length; 
+            //reset
+            pair.match = false;
+        });
+        return matches != this.selection.length;
     }
-        
-    private hasCompleteSelection = () =>{ 
-        var answer = true; 
+
+    private hasCompleteSelection = () =>{
+        var answer = true;
         angular.forEach(this.selection, (pair)=>{
             if(pair.optionID.length === 0){
-                answer = false; 
+                answer = false;
             }
         });
         return answer;
     }
-    
-    private addToSelection = (optionId:string, optionGroupId:string) => { 
+
+    private addToSelection = (optionId:string, optionGroupId:string) => {
         angular.forEach(this.selection, (pair)=>{
             if(pair.optionGroupID === optionGroupId){
-                pair.optionID = optionId; 
-                return true; 
+                pair.optionID = optionId;
+                return true;
             }
         });
-        return false; 
+        return false;
     }
-    
+
 }
 
 class SWAddOptionGroup implements ng.IDirective{
 
-    public templateUrl; 
-    public restrict = "EA"; 
-    public scope = {}	
-    
+    public templateUrl;
+    public restrict = "EA";
+    public scope = {}
+
     public bindToController = {
-        productId:"@", 
+        productId:"@",
         optionGroups:"="
     }
     public controller=SWAddOptionGroupController;
     public controllerAs="swAddOptionGroup";
-     
+
     public static Factory():ng.IDirectiveFactory{
 		var directive:ng.IDirectiveFactory = (
-            $hibachi, 
-            $timeout, 
+            $hibachi,
+            $timeout,
             collectionConfigService,
             observerService,
 			optionGroupPartialsPath,
 			slatwallPathBuilder
 		) => new SWAddOptionGroup(
-            $hibachi, 
-            $timeout, 
+            $hibachi,
+            $timeout,
             collectionConfigService,
             observerService,
 			optionGroupPartialsPath,
 			slatwallPathBuilder
 		);
 		directive.$inject = [
-            '$hibachi', 
-            '$timeout', 
+            '$hibachi',
+            '$timeout',
             'collectionConfigService',
             'observerService',
 			'optionGroupPartialsPath',
@@ -204,14 +211,14 @@ class SWAddOptionGroup implements ng.IDirective{
 		return directive;
 	}
     // @ngInject
-    constructor(private $hibachi, private $timeout, 
-                private collectionConfigService, 
+    constructor(private $hibachi, private $timeout,
+                private collectionConfigService,
                 private observerService, private optionGroupPartialsPath, slatwallPathBuilder
     ){
-        this.templateUrl = slatwallPathBuilder.buildPartialsPath(optionGroupPartialsPath) + "addoptiongroup.html";	
+        this.templateUrl = slatwallPathBuilder.buildPartialsPath(optionGroupPartialsPath) + "addoptiongroup.html";
     }
 
-    public link:ng.IDirectiveLinkFn = ($scope: ng.IScope, element: ng.IAugmentedJQuery, attrs:ng.IAttributes) =>{  
+    public link:ng.IDirectiveLinkFn = ($scope: ng.IScope, element: ng.IAugmentedJQuery, attrs:ng.IAttributes) =>{
     }
 }
 
