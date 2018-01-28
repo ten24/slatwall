9ffcee7804d85e7ad0a3d8fb586f12f38893db0f
ian-a-hickey <ian.hickey@ten24web.com>
2018-01-28 17:02:19 -0500

    saving

---
+++ admin/client/testing.spec.ts
@@ -0,0 +1,7 @@
+console.info('I am running bro...');
+describe('test', () => {
+    it('should expect nothing', () => {
+        console.info('whatever AA');
+        expect(true).toBe(true);
+    });
+});
