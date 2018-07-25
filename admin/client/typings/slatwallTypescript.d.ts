bbdcb4bf666a6fcfd9e2e994020c65a7e56394a8
Ryan Marchand <ryan.marchand@ten24web.com>
2018-07-25 17:32:52 +0000

    updating typings

--- admin/client/typings/slatwallTypescript.d.ts
+++ admin/client/typings/slatwallTypescript.d.ts
@@ -1,13 +1,9 @@
 //{
 /// <reference path="../src/bootstrap.ts" />
 /// <reference path="../src/test.ts" />
-<<<<<<< HEAD
-/// <reference path="../src/formbuilder/formbuilder.module.ts" />
-/// <reference path="../src/fulfillmentbatch/fulfillmentbatchdetail.module.ts" />
-=======
 /// <reference path="../src/content/content.module.ts" />
 /// <reference path="../src/formbuilder/formbuilder.module.ts" />
->>>>>>> 28db19d4351c08d95267255885ed6c8e93690c4c
+/// <reference path="../src/fulfillmentbatch/fulfillmentbatchdetail.module.ts" />
 /// <reference path="../src/giftcard/giftcard.module.ts" />
 /// <reference path="../src/optiongroup/optiongroup.module.ts" />
 /// <reference path="../src/orderfulfillment/orderfulfillment.module.spec.ts" />
@@ -24,12 +20,9 @@
 /// <reference path="../src/content/components/swcontentnode.ts" />
 /// <reference path="../src/content/components/swsiteselector.ts" />
 /// <reference path="../src/formbuilder/components/swformresponselisting.ts" />
-<<<<<<< HEAD
 /// <reference path="../src/fulfillmentbatch/actions/fulfillmentbatchactions.ts" />
 /// <reference path="../src/fulfillmentbatch/components/swfulfillmentbatchdetail.ts" />
 /// <reference path="../src/fulfillmentbatch/components/swfulfillmentbatchlist.spec.ts" />
-=======
->>>>>>> 28db19d4351c08d95267255885ed6c8e93690c4c
 /// <reference path="../src/giftcard/components/swaddorderitemgiftrecipient.ts" />
 /// <reference path="../src/giftcard/components/swgiftcardbalance.ts" />
 /// <reference path="../src/giftcard/components/swgiftcarddetail.ts" />
