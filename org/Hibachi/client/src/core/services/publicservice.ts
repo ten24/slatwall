1ae604d729e368ecc02318430fb967a0ebc0dc53
Ryan Marchand <ryan.marchand@ten24web.com>
2018-08-06 17:42:21 +0000

    bug fix in typescript

--- org/Hibachi/client/src/core/services/publicservice.ts
+++ org/Hibachi/client/src/core/services/publicservice.ts
@@ -1290,7 +1290,7 @@ class PublicService {
     
     public getOrderAttributeValues = (allowedAttributeSets) =>{
         var attributeValues = {};
-        var orderAttributeModel = JSON.parse(localStorage.attributeMetaData)["Order"];
+        var orderAttributeModel = JSON.parse(localStorage.getItem('attributeMetaData'))["Order"];
         for(var attributeSetCode in orderAttributeModel){
             var attributeSet = orderAttributeModel[attributeSetCode];
             if(allowedAttributeSets.indexOf(attributeSetCode) !== -1){
