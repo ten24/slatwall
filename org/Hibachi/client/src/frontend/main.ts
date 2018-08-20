b287847ae94b7fb9fe99a3cb5386b6181ffb9458
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-08-20 16:27:29 +0530

    created main.ts file and update the webpack-frontend-devlop.config.js file for entry app
    created main.ts file and update the webpack-frontend-devlop.config.js file for entry app

---
+++ org/Hibachi/client/src/frontend/main.ts
@@ -0,0 +1,9 @@
+/*jshint browser:true */
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { FrontendModule } from "./frontend.module";
+
+platformBrowserDynamic().bootstrapModule(FrontendModule);
+
+
