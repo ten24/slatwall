ceb8e36a43c69ca1685d8509f5fa2bb8091b985a
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-09-11 13:03:47 +0530

    update the file swlistingdisplay ( solved error at content page )
    update the file swlistingdisplay ( solved error at content page )

--- org/Hibachi/client/src/listing/components/swlistingdisplay.ts
+++ org/Hibachi/client/src/listing/components/swlistingdisplay.ts
@@ -226,8 +226,10 @@ class SWListingDisplayController{
 
         this.listingService.setListingState(this.tableID, this);
         
-        if(this.collectionConfig.keywords && this.collectionConfig.keywords.length){
-            this.searchText = this.collectionConfig.keywords;
+        if(this.collectionConfig ) {
+            if(this.collectionConfig.keywords && this.collectionConfig.keywords.length){
+                this.searchText = this.collectionConfig.keywords;
+            }
         }
 
         //this is performed after the listing state is set above to populate columns and multiple collectionConfigs if present
