a369ce0b02c990838878a47fdd7c8b8f0172bc13
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-10-18 18:09:52 +0530

    upgraded the swOrderItemDetailStamp into angular 2 and update the html for data binding in orderitem
    upgraded the swOrderItemDetailStamp into angular 2 and update the html for data binding in orderitem

--- admin/client/src/orderitem/components/sworderitemdetailstamp.ts
+++ admin/client/src/orderitem/components/sworderitemdetailstamp.ts
@@ -45,6 +45,7 @@ class SWOrderItemDetailStamp{
 				orderItem:"="
 			},
 			templateUrl:slatwallPathBuilder.buildPartialsPath(orderItemPartialsPath)+"orderitem-detaillabel.html",
+            
 			link: function(scope, element, attrs){
 				scope.details = [];
 				scope.orderItem.detailsName = [];
@@ -133,4 +134,108 @@ class SWOrderItemDetailStamp{
 }
 export{
 	SWOrderItemDetailStamp
+}
+
+import { Component, OnInit, Input } from '@angular/core';
+import { $Hibachi } from '../../../../../org/Hibachi/client/src/core/services/hibachiservice';
+import { CollectionConfig } from '../../../../../org/Hibachi/client/src/collection/services/collectionconfigservice';
+
+@Component({
+    selector   : 'sw-order-item-detail-stamp',
+    templateUrl: '/admin/client/src/orderitem/components/orderitem-detaillabel.html'   
+})
+export class SwOrderItemDetailStamp implements OnInit {
+        
+    @Input() systemcode: any;
+    @Input() orderitemid: string;
+    @Input() skuid: string;
+    @Input() orderitem: any;
+    
+    constructor(
+        private hibachi : $Hibachi,
+        private collectionConfigService: CollectionConfig
+        
+    ) {
+        
+    }
+    
+    ngOnInit() {
+
+        if( angular.isUndefined(this.orderitem.detailsName) ) {
+            this.orderitem.detailsName = [];
+        }
+        
+        if ( angular.isUndefined(this.orderitem.details)){
+            this.orderitem.details = [];
+        }
+        
+        if (angular.isDefined(this.orderitem.details)){
+                    
+            switch (this.systemcode){
+                case "merchandise":
+                    this.getMerchandiseDetails(this.orderitem);
+                    break;
+                case "subscription":
+                    this.getSubscriptionDetails(this.orderitem);
+                    break;
+                case "event":
+                    this.getEventDetails(this.orderitem);
+                    break;
+            }
+       }            
+    }
+    
+    /**
+      * For each type of orderItem, get the appropriate detail information.
+      *
+      * Merchandise: Option Group Name and Option
+      * Event: Event Date, Event Location
+      * Subscription: Subscription Term, Subscription Benefits
+      */
+    getMerchandiseDetails = function(orderItem){
+        //Get option and option groups
+        for (var i = 0; i <=  orderItem.data.sku.data.options.length - 1; i++){
+            var optionGroupCollectionConfig = this.collectionConfigService.newCollectionConfig("Option");
+            optionGroupCollectionConfig.addDisplayProperty("optionID,optionName, optionGroup.optionGroupName");
+            optionGroupCollectionConfig.addFilter("optionID", orderItem.data.sku.data.options[i].optionID, "=");
+            optionGroupCollectionConfig.getEntity().then(
+                (results)=>{
+                    if(angular.isDefined(results.pageRecords[0])){
+                        orderItem.detailsName.push(results.pageRecords[0].optionGroup_optionGroupName);
+                        orderItem.details.push(results.pageRecords[0].optionName);
+                    }
+                },
+                (reason)=>{
+                    throw("SWOrderItemDetailStamp had trouble retrieving the option group for option");
+                }
+            );
+       }
+    };
+
+    getSubscriptionDetails = function(orderItem){
+        
+        //get Subscription Term and Subscription Benefits
+        var name = orderItem.data.sku.data.subscriptionTerm.data.subscriptionTermName || "";
+        orderItem.detailsName.push("Subscription Term:");
+        orderItem.details.push(name);
+
+        //Maybe multiple benefits so show them all.
+        for (var i = 0; i <=  orderItem.data.sku.data.subscriptionBenefits.length - 1; i++){
+            var benefitName = orderItem.data.sku.data.subscriptionBenefits[i].subscriptionBenefitName || "";
+                orderItem.detailsName.push("Subscription Benefit:");
+                orderItem.details.push(benefitName);
+         }
+    };
+
+    getEventDetails = function(orderItem){
+        //get event date, and event location
+        orderItem.detailsName.push("Event Date: ");
+        orderItem.details.push(orderItem.data.sku.data.eventStartDateTime);
+        //Need to iterate this.
+        for (var i = 0; i <= orderItem.data.sku.data.locations.length - 1; i++ ){
+            orderItem.detailsName.push("Location: ");
+            orderItem.details.push(orderItem.data.sku.data.locations[i].locationName);
+        }
+    };
+    
 }
--- admin/client/src/orderitem/orderitem.module.ts
+++ admin/client/src/orderitem/orderitem.module.ts
@@ -6,23 +6,28 @@ import {CoreModule} from '../../../../org/Hibachi/client/src/core/core.module';
 
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {UpgradeModule,downgradeInjectable} from '@angular/upgrade/static';
+import {UpgradeModule,downgradeInjectable,downgradeComponent} from '@angular/upgrade/static';
 
 //directives
 import {SWChildOrderItem} from "./components/swchildorderitem";
 import {SWOrderItem} from "./components/sworderitem";
 import {SWOiShippingLabelStamp} from "./components/swoishippinglabelstamp";
-import {SWOrderItemDetailStamp} from "./components/sworderitemdetailstamp";
+import {SWOrderItemDetailStamp, SwOrderItemDetailStamp} from "./components/sworderitemdetailstamp";
 import {SWOrderItems} from "./components/sworderitems";
 import {SWResizedImage} from "./components/swresizedimage";
 
 @NgModule({
-    declarations :[],
+    declarations :[
+        SwOrderItemDetailStamp
+    ],
     providers: [],
     imports : [
         CoreModule,
         CommonModule,
         UpgradeModule
+    ],
+    entryComponents: [
+        SwOrderItemDetailStamp
     ]
 })
 export class OrderItemModule{
@@ -41,7 +46,8 @@ var orderitemmodule = angular.module('hibachi.orderitem', [coremodule.name])
 .directive('swChildOrderItem',SWChildOrderItem.Factory())
 .directive('swOrderItem',SWOrderItem.Factory())
 .directive('swoishippinglabelstamp',SWOiShippingLabelStamp.Factory())
-.directive('swOrderItemDetailStamp',SWOrderItemDetailStamp.Factory())
+//.directive('swOrderItemDetailStamp',SWOrderItemDetailStamp.Factory())
+.directive('swOrderItemDetailStamp', downgradeComponent({ component: SwOrderItemDetailStamp }) as angular.IDirectiveFactory)    
 .directive('swOrderItems',SWOrderItems.Factory())
 .directive('swresizedimage',SWResizedImage.Factory())
 //constants
