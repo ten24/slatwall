380aa9a97ab3965339855555f1746de76319c371
Denival Heitz <denival.heitz@ten24web.com>
2019-03-26 15:39:29 +0000

    added custom/admin/client to list of paths to resolve

--- admin/client/webpack.config.js
+++ admin/client/webpack.config.js
@@ -17,5 +17,5 @@ devConfig.context = PATHS.app;
 //don't need the vendor bundle generated here because we include the vendor bundle already.
 devConfig.plugins =  [
 ];
-devConfig.resolve.modules=[path.resolve(path.join(customPath, '../../custom/client'), 'src/'),path.resolve(__dirname, 'src/'),'node_modules'];
+devConfig.resolve.modules=[path.resolve(path.join(customPath, '../../custom/admin/client'), 'src/'),path.resolve(path.join(customPath, '../../custom/client'), 'src/'),path.resolve(__dirname, 'src/'),'node_modules'];
 module.exports = devConfig;
