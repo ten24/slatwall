98af273dd283d8dcd1070c43d2a3fe4c69a6de9b
Nitin Yadav <nitin.yadav@ten24web.com>
2019-04-03 10:08:39 +0000

    more type fixes

--- org/Hibachi/client/src/core/services/formservice.ts
+++ org/Hibachi/client/src/core/services/formservice.ts
@@ -10,6 +10,8 @@ class Form implements ng.IFormController{
     public $invalid: boolean;
     public $submitted: boolean;
     public $error: any;
+    public $pending: any;
+
     $addControl = (control: ng.INgModelController): void =>{}
     $removeControl = (control: ng.INgModelController): void =>{}
     $setValidity = (validationErrorKey: string, isValid: boolean, control: ng.INgModelController): void =>{}
