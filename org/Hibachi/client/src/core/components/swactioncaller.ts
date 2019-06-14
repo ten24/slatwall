5da230c649cf7eec7f8aaad2828ff9f6f68f96e2
Ryan Marchand <ryan.marchand@ten24web.com>
2019-06-14 14:29:17 +0000

    adding typing

--- org/Hibachi/client/src/core/components/swactioncaller.ts
+++ org/Hibachi/client/src/core/components/swactioncaller.ts
@@ -23,6 +23,7 @@ class SWActionCallerController{
     public formController:any;
     public form:ng.IFormController;
     public authenticateActionByAccount:any;
+    public actionAuthenticated:boolean;
     //@ngInject
     constructor(
         private $scope,
