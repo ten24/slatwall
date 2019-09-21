9c78edf7f1c4a051e5ef277ca5f7a73a4eebbea5
Tom Rodrick <tom.rodrick@ten24web.com>
2019-09-21 14:01:19 +0000

    controller for further use

---
+++ custom/client/src/monatfrontend/components/monatenrollmentvip.ts
@@ -0,0 +1,42 @@
+class VIPController {
+    public Account_CreateAccount
+
+    // @ngInject
+    constructor(
+        public $rootScope,
+        public $scope,
+    ){}
+    
+}
+
+class MonatEnrollmentVIP  {
+    
+    
+    public require          = {
+        ngModel:'?^ngModel'    
+    };
+    public priority         =1000;
+    public restrict         = "A";
+    public scope            = true;
+   /**
+    * Binds all of our variables to the controller so we can access using this
+    */
+    public bindToController = {
+    };
+    public controller       = VIPController;
+    public controllerAs     = "vipController";
+    // @ngInject
+    constructor() {
+    }
+
+    public static Factory(){
+        var directive = () => new MonatEnrollmentVIP();
+        directive.$inject = [];
+        return directive;
+    }
+    
+}
+export{
+    MonatEnrollmentVIP,
+    VIPController
+}
