c0d2a3c85206454d11c459bd9a7981c8943c8d97
Tom Rodrick <tom.rodrick@ten24web.com>
2019-11-04 22:01:26 +0000

    https://ten24.teamwork.com/index.cfm#/tasks/26532944

--- custom/client/src/monatfrontend/components/monatflexshiplisting.ts
+++ custom/client/src/monatfrontend/components/monatflexshiplisting.ts
@@ -10,6 +10,7 @@ class MonatFlexshipListingController{
 	public expirationMonthOptions: any[];
 	public expirationYearOptions: any[];
 	public loading: boolean = false;
+	public daysToEditFlexshipSetting:any;
 	
 		
 	private orderTemplateTypeID:string = '2c948084697d51bd01697d5725650006'; // order-template-type-flexship 
@@ -17,12 +18,15 @@ class MonatFlexshipListingController{
 	public initialized = false; 
 	
 	//@ngInject
-	constructor(public orderTemplateService, public $window){
+	constructor(public orderTemplateService, public $window, public publicService){
 		
 	}
 	
 	public $onInit = () => {
 		this.fetchFlexships();
+		this.orderTemplateService.getOrderTemplateSettings().then(data =>{
+			this.daysToEditFlexshipSetting = data.orderTemplateSettings;
+		})
 	}
 	
 	private fetchFlexships = () => {
@@ -30,6 +34,7 @@ class MonatFlexshipListingController{
 		this.orderTemplateService
     		.getOrderTemplates(this.orderTemplateTypeID )
 			.then( (data) => {
+
 				this.accountAddresses = data.accountAddresses;
 				this.accountPaymentMethods = data.accountPaymentMethods;
 				this.shippingMethodOptions = data.shippingMethodOptions;
