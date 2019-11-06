54aa1657a56e4f2ae0ab3a41b711dcf8f88c7f60
Tom Rodrick <tom.rodrick@ten24web.com>
2019-11-06 22:37:28 +0000

    ellipses logic mostly working

--- custom/client/src/monatfrontend/components/swfpagination.ts
+++ custom/client/src/monatfrontend/components/swfpagination.ts
@@ -10,35 +10,50 @@ class SWFPaginationController {
     public totalPageArray:Array<any>;
     public productList:any;
     public argumentsObject:any;
-    
+    public beginPaginationAt:number;
+    public displayPages:any;
+    public elipsesNum;
 	// @ngInject
-	constructor(
-		//inject modal service
-		public orderTemplateService,
-		public monatService,
-        public observerService,
-        public $scope,
-        public publicService
-	) { 
+	constructor(public observerService, public $scope,public publicService) { 
         this.observerService.attach(this.init,"PromiseComplete"); 
-
-	}
+        if(this.beginPaginationAt){
+            this.elipsesNum = this.beginPaginationAt;
+        }else {
+            this.elipsesNum = 10;
+        }
+	};
 	
 	public init = () => {
         this.totalPages = Math.ceil(this.recordsCount / this.itemsPerPage);
         const holdingArray = [];
+        const holdingDisplayPagesArray = [];
+
         for(var i = 0; i <= this.totalPages -1; i++){
             holdingArray.push(i);
+            if(i <= this.elipsesNum){
+                holdingDisplayPagesArray.push(i);
+            }
         }
+        this.displayPages = holdingDisplayPagesArray;
         this.totalPageArray = holdingArray;
 	}
 	
-    public getNextPage = ( pageNumber = 1, direction:any = false) => {
-
+    public getNextPage = ( pageNumber = 1, direction:any = false, newPages = false) => {
+		let setNew;
+		
+		if (pageNumber === 1) {
+			setNew = true;
+		}
+		
+        //direction logic
         if(direction === 'prev'){
+			setNew = false;
             if(this.pageTracker === 1){
                 return pageNumber;
-            }else{
+            }else if(this.pageTracker == this.displayPages[0]) {
+                newPages = true;
+            }
+            else{
                 pageNumber = this.pageTracker -1;
             }
         }else if(direction === 'next'){
@@ -49,7 +64,26 @@ class SWFPaginationController {
                 pageNumber = this.pageTracker +1;
             }
         }
-        
+        //END: direction logic
+
+        //Ellipses Logic
+		if (newPages) {
+		    debugger;
+			pageNumber = (direction == 'prev') ? this.pageTracker -1 : this.displayPages.length;
+			let manipulatePageNumber = pageNumber;
+			this.displayPages = [];
+			for(let i = 0; ; i++){
+			   if( i >= this.elipsesNum || manipulatePageNumber >= this.totalPageArray.length){ break; }
+			   if(direction == 'prev'){
+			      this.displayPages.unshift(manipulatePageNumber--); 
+			   }else{
+			       this.displayPages.push(manipulatePageNumber++);
+			   }
+			}
+			console.log(this.displayPages);
+		}
+        //END: Ellipses Logic
+
         this.argumentsObject['pageRecordsShow'] = this.itemsPerPage;
         this.argumentsObject['currentPage'] = pageNumber;
         
@@ -74,7 +108,6 @@ class SWFPagination {
 		productList:'=', //Sets up two way binding so concurrent API responses overwrite it
 		argumentsObject:'<?', //optional object of arguments to pass in to the api call
 		beginPaginationAt:'@?' //this can be left blank unless the user wants the "..." pagination to begin at a number other than 11
-
 	};
 
 	public controller = SWFPaginationController;
