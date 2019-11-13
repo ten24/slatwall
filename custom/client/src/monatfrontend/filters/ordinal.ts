dcfba13d1e74790fbd6460fc15c51a8fbf1aadeb
Storm Rockwell <storm.rockwell@ten24web.com>
2019-11-13 18:19:50 +0000

    spacing

--- custom/client/src/monatfrontend/filters/ordinal.ts
+++ custom/client/src/monatfrontend/filters/ordinal.ts
@@ -2,9 +2,9 @@ class OrdinalFilter {
 	public static Factory() {
 		return function( input ) {
 			var suffixes = ['th', 'st', 'nd', 'rd'];
-		    var relevantDigits = ( input < 30 ) ? input % 20 : input % 30;
-		    var suffix = ( relevantDigits <= 3 ) ? suffixes[ relevantDigits ] : suffixes[0];
-		    return input + suffix;
+			var relevantDigits = ( input < 30 ) ? input % 20 : input % 30;
+			var suffix = ( relevantDigits <= 3 ) ? suffixes[ relevantDigits ] : suffixes[0];
+			return input + suffix;
 		}
 	}
 }
