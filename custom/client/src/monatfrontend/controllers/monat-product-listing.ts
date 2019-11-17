2fa79853756cc8b0954f1142e636058f44ffca11
Tom Rodrick <tom.rodrick@ten24web.com>
2019-11-17 18:06:43 +0000

    pagination

--- custom/client/src/monatfrontend/controllers/monat-product-listing.ts
+++ custom/client/src/monatfrontend/controllers/monat-product-listing.ts
@@ -7,6 +7,8 @@ class MonatProductListingController {
     public cmsCategoryFilterFlag:boolean;
     public contentFilterFlag:boolean;
     public cmsContentFilterFlag:boolean;
+    public pageRecordsShow:number = 12;
+    public recordsCount:number;
 
 	// @ngInject
 	constructor(
@@ -24,28 +26,30 @@ class MonatProductListingController {
         
         // CMS category ID is the only filter applied via ng-init and getting the CF category, due to content modules loop
         // All others are handled with headers, and only need the flag 
-        
         if(this.cmsContentID.length && this.cmsContentFilterFlag) { 
             this.argumentsObject['cmsContentID'] = this.cmsContentID;  
             this.argumentsObject['cmsContentFilterFlag'] = true;  
         }
 
         if(this.contentFilterFlag) this.argumentsObject['contentFilterFlag'] = true;  
-        if(this.cmsCategoryFilterFlag == true) this.argumentsObject['cmsCategoryFilterFlag'] = true;  
-
+        if(this.cmsCategoryFilterFlag) this.argumentsObject['cmsCategoryFilterFlag'] = true; 
+        this.argumentsObject['pageRecordsShow'] = this.pageRecordsShow;
+        
         this.publicService.doAction('getProductsByCategoryOrContentID', this.argumentsObject).then(result => {
             this.productList = result.productList;
+            this.recordsCount = result.recordsCount;
+			this.observerService.notify('PromiseComplete');
             this.loading = false;
         })
     }
 }
 /*********************
     To use: 
-    1. Declare as controller of a wrapper div
-    2. ng-init a flag if you would like to filter on it 
+    1. Declare as the controller of a wrapper div
+    2. ng-init a filter flag as true if you would like it applied,
         the corresponding ID is already being sent in the headers (with exception of cmsContentID for content-module loops, this will need an ng-init)
     3. ng-repeat using monat-product-card
-    4. Use pagination directive if you would like to paginate
+    4. Use pagination directive and pass in the controllers paginationArguments if you would like to paginate
     
 *******************/
 
