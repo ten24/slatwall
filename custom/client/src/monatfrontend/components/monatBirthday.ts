2061eb5a82600b46d4e624d79b6bdb82d212ee81
Tom Rodrick <tom.rodrick@ten24web.com>
2020-02-24 17:10:46 +0000

    debouncing days

--- custom/client/src/monatfrontend/components/monatBirthday.ts
+++ custom/client/src/monatfrontend/components/monatBirthday.ts
@@ -1,5 +1,11 @@
 type plusOrMinus = '+' | '-';
 
+enum ActionType {
+	PLUS,
+	MINUS,
+	INPUT
+}
+
 class MonatBirthdayController {
 	public showPicker = false;
 	public date = new Date();
@@ -12,42 +18,79 @@ class MonatBirthdayController {
 	public downMonth = this.months[this.months.indexOf(this.month) - 1];
 	public upDay = 2;
 	public downDay = new Date(this.date.getFullYear(), this.months.indexOf(this.month)+1, 0).getDate();
+	public Actions = ActionType;
 	
 	//@ngInject
 	constructor(public monatService, public observerService, public $rootScope, public publicService, public $scope) {}
 
 	public $onInit = () => {
-
+		
 	}
 	
 	public showBirthdayPicker():void{
 		this.showPicker = !this.showPicker;
 	}
 	
-	public changeMonth(action: plusOrMinus):void{
+	public changeMonth(action: ActionType):void{
+	
 		this.month = this.getAdjustedMonth(action); 
-		this.upMonth = this.getAdjustedMonth('+');
-		this.downMonth = this.getAdjustedMonth('-');
+		this.upMonth = this.getAdjustedMonth(ActionType.PLUS);
+		this.downMonth = this.getAdjustedMonth(ActionType.MINUS);
 		this.resetModel();
 	}
 	
-	public changeDay(action: plusOrMinus):void{
+	public changeDay(action: ActionType):void{
+
 		//disallow days below 1, and days above the last day in the month
 		this.day = this.getAdjustedDay(action);
-		this.upDay = this.getAdjustedDay('+');
-		this.downDay = this.getAdjustedDay('-');
+		this.upDay = this.getAdjustedDay(ActionType.PLUS);
+		this.downDay = this.getAdjustedDay(ActionType.MINUS);
 		this.resetModel()
 	}
 	
-	public changeYear(action: plusOrMinus):void{
+	public changeYear(action: ActionType):void{
+		this.year = +this.year;
 		//disallow years above the current year
-		if(action === '+' && this.year < this.currentYear){
+		if(action === ActionType.PLUS && this.year < this.currentYear){
 			this.year += 1;
 			this.resetModel();
-		}else if(action === '-'){
+		}else if(action === ActionType.MINUS){
 			this.year -= 1;
 			this.resetModel();
 		}
+	}
+	
+	public getAdjustedMonth(action:ActionType):string{
+		if(this.months.indexOf(this.month) == this.months.length -1 && action === ActionType.PLUS){
+			return this.months[0];
+		}else if(this.months.indexOf(this.month) === 0 && action === ActionType.MINUS){
+			return this.months[this.months.length -1]
+		}else{
+			return (action === ActionType.PLUS) ? this.months[this.months.indexOf(this.month) + 1] : this.months[this.months.indexOf(this.month) - 1];
+		}
+	}
+	
+	public getAdjustedDay(action:ActionType):number{
+		this.day = +this.day;
+		let daysInCurrentMonth = new Date(this.date.getFullYear(), this.months.indexOf(this.month)+1, 0).getDate();
+		
+		if(action === ActionType.INPUT){
+			if(this.day > daysInCurrentMonth){
+				return daysInCurrentMonth;
+			}else if(this.day === 0 ){
+				return 1;
+			}else{
+				return this.day;
+			}
+		}
+		
+		if(this.day === 1 && action ===ActionType.MINUS){
+			return daysInCurrentMonth;
+		} else if(this.day === daysInCurrentMonth && action === ActionType.PLUS){
+			return 1;
+		}
+		
+		return (action === ActionType.PLUS) ? this.day + 1 : this.day - 1;
 	}
 	
 	public resetModel(){
@@ -56,28 +99,6 @@ class MonatBirthdayController {
 		this.$scope.swfForm.form.year = {$modelValue: this.year};
 		this.$scope.swfForm.form.day = {$modelValue: this.day};
 	}
-	
-	public getAdjustedMonth(action:plusOrMinus):string{
-		if(this.months.indexOf(this.month) == this.months.length -1 && action === '+'){
-			return this.months[0];
-		}else if(this.months.indexOf(this.month) === 0 && action === '-'){
-			return this.months[this.months.length -1]
-		}else{
-			return (action === '+') ? this.months[this.months.indexOf(this.month) + 1] : this.months[this.months.indexOf(this.month) - 1];
-		}
-	}
-	
-	public getAdjustedDay(action:plusOrMinus):number{
-		let daysInCurrentMonth = new Date(this.date.getFullYear(), this.months.indexOf(this.month)+1, 0).getDate();
-		if(+this.day === 1 && action ==='-'){
-			return daysInCurrentMonth;
-		} else if(+this.day === daysInCurrentMonth && action === '+'){
-			return 1;
-		}
-		
-		return (action === '+') ? this.day + 1 : this.day - 1;
-	}
-	
 	
 }
 
