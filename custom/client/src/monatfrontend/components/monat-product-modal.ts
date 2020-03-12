ca7d084e1104bfb7091f5aaefce209f551ac2863
Tom Rodrick <tom.rodrick@ten24web.com>
2020-03-12 03:10:52 +0000

    https://ten24.teamwork.com/index.cfm#/tasks/28183487

--- custom/client/src/monatfrontend/components/monat-product-modal.ts
+++ custom/client/src/monatfrontend/components/monat-product-modal.ts
@@ -46,36 +46,7 @@ class MonatProductModalController {
 			let content = <HTMLDivElement>document.getElementById('product-modal')!;
 			let parser = new DOMParser();
 			let doc = parser.parseFromString(res.data, "text/html");
-			
-			//Removing the header, footer and instagram feed
-			let footer = <HTMLElement><any>doc.getElementsByTagName('footer');
-			let header = doc.getElementsByTagName('header');
-			let feed = doc.getElementsByClassName("instagram-feed");
-			let iconRow = doc.getElementsByClassName("iconed-featurs");
-			
-			feed[0].parentNode.removeChild(feed[0]);
-			footer[0].parentNode.removeChild(footer[0]);
-			header[0].parentNode.removeChild(header[0]);
-			iconRow[0].parentNode.removeChild(iconRow[0]);
-			
-			//wrapping the sections in accordion divs
-			let favoriteSection = doc.getElementsByClassName('favorite-section')![0];
-			if(favoriteSection){
-				favoriteSection.setAttribute("id", "favorite-section");
-				favoriteSection.classList.add("collapse");
-				let favoriteSectionParent = favoriteSection.parentElement;
-				let favoriteSectionToggelerTemplate = document.getElementsByTagName("template")[0].content.cloneNode(true);
-				favoriteSectionToggelerTemplate = (<HTMLDivElement>favoriteSectionToggelerTemplate.childNodes[0]).nextElementSibling;
-				(<HTMLDivElement>favoriteSectionToggelerTemplate).setAttribute("data-target", "#favorite-section");
-				(<HTMLDivElement>favoriteSectionToggelerTemplate).setAttribute("data-toggle", "collapse");
-				(<HTMLDivElement>favoriteSectionToggelerTemplate).getElementsByClassName('title-paragraph')[0].innerHTML= "Benefits"; //rb key
-				(<HTMLDivElement>favoriteSectionToggelerTemplate).getElementsByClassName('more-paragraph')[0].innerHTML= "View More"; //rb key
-				let favoriteSectionAccordion = doc.createElement('div');
-				favoriteSectionAccordion.appendChild(favoriteSection);
-				favoriteSectionParent.appendChild(favoriteSectionAccordion);
-				favoriteSectionParent.insertBefore(favoriteSectionToggelerTemplate, favoriteSectionAccordion);
-			}
-			content.appendChild(doc.getElementById('wrapper'));
+			content.innerHTML =res.data;
 		});
 	};
 	
@@ -149,11 +120,12 @@ class MonatProductModalController {
 		});
 	};
 
-	public addToCart = () => {
+	public addToCart = (skuID= this.product.skuID,  quantity = this.quantityToAdd) => {
+		console.log('clickjjed')
 		this.loading = true;
 		this.monatService.addToCart(
-			this.product.skuID, 
-			this.quantityToAdd
+			skuID, 
+			quantity
 		)
 		.then((data) => {
 			this.monatAlertService.success("Product added to cart successfully");
