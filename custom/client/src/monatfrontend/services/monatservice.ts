21b5cee158e8cea6bc9083f384b7a7fdb02d0ca9
Nitin Yadav <nitin.yadav@ten24web.com>
2020-04-02 14:56:12 +0000

    Update: monate-service, add/update get-xxx-options APIs, add: updated package.json

--- custom/client/src/monatfrontend/services/monatservice.ts
+++ custom/client/src/monatfrontend/services/monatservice.ts
@@ -1,8 +1,11 @@
-interface IOptions {
+import { Cache } from 'cachefactory';
+
+export interface IOption {
 	name: string;
 	value: any;
 }
 
+declare var $: any;
 declare var angular: any;
 declare var hibachiConfig: any;
 
@@ -11,15 +14,38 @@ export class MonatService {
 	public lastAddedSkuID: string = '';
 	public previouslySelectedStarterPackBundleSkuID:string;
 	public canPlaceOrder:boolean;
+
 	public cachedOptions = {
-		frequencyTermOptions: <IOptions[]>null,
-		countryCodeOptions: <IOptions[]>null,
+		
+		//flexship related options
+		frequencyTermOptions: <IOption[]>null,
+		frequencyDateOptions: <IOption[]>null,
+		cancellationReasonTypeOptions: <IOption[]>null,
+		scheduleDateChangeReasonTypeOptions: <IOption[]>null,
+		expirationMonthOptions: <IOption[]>null,
+		expirationYearOptions: <IOption[]>null,
+		
+		//other generic options
+		countryCodeOptions: <IOption[]>null,
 	};
+	
 	public userIsEighteen:boolean;
 	public hasOwnerAccountOnSession:boolean;
 	
 	//@ngInject
-	constructor(public publicService, public $q, public $window, public requestService, private observerService) {}
+	constructor(
+		private $q, 
+		private $window, 
+		private publicService, 
+		private requestService, 
+		private observerService,
+		private utilityService,
+	) {
+		
+		// console.log("localCache keys: "+this.localStorageCache.keys());
+		// console.log("sessionCache keys: "+this.sessionStorageCache.keys());
+		// console.log("memoryCache keys: "+this.memoryCache.keys());
+	}
 
 	public getCart(refresh = false, param = '') {
 		var deferred = this.$q.defer();
@@ -43,8 +69,7 @@ export class MonatService {
 	/**
 	 * actions => addOrderItem, removeOrderItem, updateOrderItemQuantity, ....
 	 *
-	 */
-
+	*/
 	private updateCart = (action: string, payload): Promise<any> => {
 		let deferred = this.$q.defer();
 		payload['returnJSONObjects'] = 'cart';
@@ -124,7 +149,7 @@ export class MonatService {
 
 	/**
 	 * options = {optionName:refresh, ---> option2:true, o3:false}
-	 */
+	*/
 	public getOptions(options: {}, refresh = false) {
 		var deferred = this.$q.defer();
 		var optionsToFetch = this.makeListOfOptionsToFetch(options, refresh);
@@ -132,7 +157,8 @@ export class MonatService {
 		if (refresh || (optionsToFetch && optionsToFetch.length)) {
 			this.requestService
 				.newPublicRequest('?slatAction=api:public.getOptions', { optionsList: optionsToFetch })
-				.promise.then((data) => {
+				.promise
+				.then((data) => {
 					var { messages, failureActions, successfulActions, ...realOptions } = data; //destructuring we dont want unwanted data in cached options
 					this.cachedOptions = { ...this.cachedOptions, ...realOptions }; // override and merge with old options
 					this.sendOptionsBack(options, deferred);
@@ -147,37 +173,76 @@ export class MonatService {
 	private makeListOfOptionsToFetch(options: {}, refresh: boolean = false) {
 		return Object.keys(options)
 			.filter((key) => refresh || !!options[key] || !this.cachedOptions[key])
-			.reduce((previous, current) => {
-				if (current) {
-					previous = previous.length ? previous + ',' + current : current;
-				}
-				return previous;
-			}, '');
+			.reduce((list, current) => {
+				return this.utilityService.listAppend(list,current);
+			});
 	}
 
 	private sendOptionsBack(options: {}, deferred) {
-		let res = Object.keys(options).reduce(
-			(result, key) => (<any>Object).assign(result, { [key]: this.cachedOptions[key] }),
-			{},
-		);
+		let res = Object.keys(options)
+			.reduce( (obj, key) => {
+				return (<any>Object).assign(obj, { [key]: this.cachedOptions[key] })
+			}, {});
 		deferred.resolve(res);
 	}
 
 	public getFrequencyTermOptions(refresh = false) {
-		var deferred = this.$q.defer();
-		if (refresh || !this.cachedOptions.frequencyTermOptions) {
-			this.requestService
-				.newPublicRequest('?slatAction=api:public.getFrequencyTermOptions')
-				.promise.then((data) => {
-					this.cachedOptions.frequencyTermOptions = data.frequencyTermOptions;
-					deferred.resolve(this.cachedOptions.frequencyTermOptions);
-				});
-		} else {
-			deferred.resolve(this.cachedOptions.frequencyTermOptions);
-		}
-		return deferred.promise;
+		return this.getOptions( {'frequencyTermOptions': refresh} );
 	}
 	
+	public getCancellationReasonTypeOptions(refresh = false) {
+		return this.getOptions( {'cancellationReasonTypeOptions': refresh} );
+	}
+	
+	public getScheduleDateChangeReasonTypeOptions(refresh = false) {
+		return this.getOptions( {'scheduleDateChangeReasonTypeOptions': refresh} );
+	}
+	
+	public getExpirationMonthOptions(refresh = false) {
+		return this.getOptions( {'expirationMonthOptions': refresh} );
+	}
+	
+	public getExpirationYearOptions(refresh = false) {
+		return this.getOptions( {'expirationYearOptions': refresh} );
+	}
+	
+	
+	
+	
+	
+
+    public countryCodeOptions = (refresh = false)=>{
+    	return this.getOptions( {'countryCodeOptions': refresh} );
+    }
+    
+    /**
+     * TODO: move to the UtilityService
+     * 
+     * This method gets the value of a cookie by its name
+     * Example cookie: "flexshipID=01234567" => "01234567"
+    **/
+    public getCookieValueByCookieName(name:string):string{
+		let cookieString = document.cookie;
+		let cookieArray = cookieString.split(';')
+		let cookieValueArray = <Array<string>>cookieArray.filter( el => el.search(name) > -1 );
+		if(!cookieValueArray.length) return '';
+    	return  cookieValueArray[0].substr(cookieValueArray[0].indexOf('=') + 1);
+    }
+    
+    /**
+    	This method takes a date string and returns age in years
+    **/
+	public calculateAge(birthDate:string):number { 
+		if(!birthDate) return;
+		let birthDateObj = <any>Date.parse(birthDate);
+	    let years = Date.now() - birthDateObj.getTime();
+	    let age = new Date(years); 
+	    let yearsOld = Math.abs(age.getUTCFullYear() - 1970);
+	    this.userIsEighteen = yearsOld >= 18;
+	    return yearsOld;
+	}
+	
+		
 	public adjustInputFocuses = () => {
 		$('input, select').focus(function() {
 			var ele = $(this);
@@ -204,47 +269,6 @@ export class MonatService {
 		}
 		
 		this.$window.location.href = redirectUrl;
-	}
-
-    public countryCodeOptions = (refresh = false)=>{
-        var deferred = this.$q.defer();
-		if (refresh || !this.cachedOptions.countryCodeOptions) {
-			this.publicService
-				.getCountries()
-				.then((data) => {
-					this.cachedOptions.countryCodeOptions = data.countryCodeOptions;
-					deferred.resolve(this.cachedOptions.countryCodeOptions);
-				});
-		} else {
-			deferred.resolve(this.cachedOptions.countryCodeOptions);
-		}
-		return deferred.promise;
-    }
-    
-    /**
-    	This method gets the value of a cookie by its name
-    	Example cookie: "flexshipID=01234567" => "01234567"
-    **/
-    
-    public getCookieValueByCookieName(name:string):string{
-		let cookieString = document.cookie;
-		let cookieArray = cookieString.split(';')
-		let cookieValueArray = <Array<string>>cookieArray.filter( el => el.search(name) > -1 );
-		if(!cookieValueArray.length) return '';
-    	return  cookieValueArray[0].substr(cookieValueArray[0].indexOf('=') + 1);
-    }
-    
-    /**
-    	This method takes a date string and returns age in years
-    **/
-	public calculateAge(birthDate:string):number { 
-		if(!birthDate) return;
-		let birthDateObj = <any>Date.parse(birthDate);
-	    let years = Date.now() - birthDateObj.getTime();
-	    let age = new Date(years); 
-	    let yearsOld = Math.abs(age.getUTCFullYear() - 1970);
-	    this.userIsEighteen = yearsOld >= 18;
-	    return yearsOld;
 	}
 
 }
