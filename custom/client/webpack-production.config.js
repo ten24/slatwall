c7dce73c2d0530c6cae91c16cd32648c12f32088
Nitin Yadav <nitin.yadav@ten24web.com>
2020-07-10 16:20:31 +0000

    Update: monat-frontend html_foot.cfm to include new bundles

--- custom/client/webpack-production.config.js
+++ custom/client/webpack-production.config.js
@@ -1,38 +1,45 @@
-var devConfig = require('./webpack.config');
-var CompressionPlugin = require("compression-webpack-plugin");
-var webpack = require('webpack');
+let devConfig = require('./webpack.config');
+const webpack = require('webpack');
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin"); //minimizer
 
-devConfig.devtool= 'none';
+const PATHS = {
+	clientRoot      :	__dirname ,
+    clientSrc       :   path.join(__dirname, '/src'),
+    monatFrontend   :	path.join(__dirname, '/src/monatfrontend'),
+    hibachiSrc      : 	path.resolve(__dirname, '../../org/Hibachi/client/src'),
+    nodeModeles     :   path.resolve(__dirname, '../../node_modules')
+};
 
-//don't need the vendor bundle generated here because we include the vendor bundle already.
-devConfig.plugins =  [
-    
-    new CompressionPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8
-    }),
-    
-    new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks
-    
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    
-    new webpack.optimize.UglifyJsPlugin({
-	    mangle: false,
-	    minimize: true,
-	    compress: {
-	         // remove warnings
-	            warnings: false,
-	
-	         // Drop console statements
-	            drop_console: true
-	       },
-	    output: {
-        	comments: false
-    	}
-	})
-];   
+
+devConfig.mode = 'production';
+devConfig.devtool = 'none';
+
+devConfig.optimization.minimizer = [
+  new TerserPlugin({
+    cache: true,
+    parallel: true,
+    extractComments: false,// will extract licenses
+    terserOptions: {
+        warnings: false,
+        parse: {},
+        compress: {
+            drop_console: true, 
+        },
+        mangle: false, // this will reduce the size of the bundles significently, but can cause problem with angular if components are not annotated properly
+        module: false,
+        output: {
+            comments: false
+        },
+        toplevel: false,
+        nameCache: null,
+        ie8: true,
+        keep_classnames: false,
+        keep_fnames: false,
+        safari10: true
+    }
+  })
+];
+
 
 module.exports = devConfig;
--- custom/client/webpack.config.js
+++ custom/client/webpack.config.js
@@ -1,46 +1,138 @@
-var devConfig = require('../../org/Hibachi/client/webpack.config');
-var CompressionPlugin = require("compression-webpack-plugin");
-var webpack = require('webpack');
-var path = require('path');
-var customPath = __dirname;
-var PATHS = {
-    app: path.join(customPath, '/src'),
-    lib: path.join(customPath, '/lib')
+const webpack = require('webpack');
+const path = require('path');
+
+const CompressionPlugin = require("compression-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const ThreadLoader = require('thread-loader');
+const NgAnnotateWebPackPlugin = require('ng-annotate-webpack-plugin');
+
+const PATHS = {
+	clientRoot      :	__dirname ,
+    clientSrc       :   path.join(__dirname, '/src'),
+    monatFrontend   :	path.join(__dirname, '/src/monatfrontend'),
+    hibachiSrc      : 	path.resolve(__dirname, '../../org/Hibachi/client/src'),
+    nodeModeles     :   path.resolve(__dirname, '../../node_modules')
 };
 
-if(typeof bootstrap !== 'undefined'){
-    devConfig.entry.app[this.entry.app.length - 1] = bootstrap;
-}
-delete devConfig.entry.vendor; //remove the vendor info from this version.
-devConfig.output.path = PATHS.app;
-devConfig.output.filename = 'monat.js';
-devConfig.context = PATHS.app;
-devConfig.devtool = 'source-map';
+const calculateNumberOfWorkers = () => {
+	const cpus = require('os').cpus() || { length: 1 };
+	return Math.max(1, cpus.length - 1);
+};
 
+const workerPool = {
+	workers: calculateNumberOfWorkers(),
+	workerParallelJobs: 5,
+	poolRespawn: process.env.NODE_ENV !== 'production',
+	poolTimeout: process.env.NODE_ENV !== 'production' ? Infinity : 2000
+};
+
+if (calculateNumberOfWorkers() > 0) {
+	ThreadLoader.warmup(workerPool, ['ts-loader']);
+}
+
+
+let devConfig = { 
+    
+    mode        : 'development',
+    devtool     : 'source-map',
+    context     : PATHS.clientSrc,
+    
+    entry: { 
+        monat: [ path.join(PATHS.clientSrc, './bootstrap.ts') ] 
+    },
+                
+    output : {
+        path:  PATHS.clientSrc,
+        filename : '[name].bundle.js'
+    },
+    
+    resolve : {
+        extensions  : [ '.webpack.js',  '.web.js', '.ts', '.js', '.html' ],
+        modules     : [ 
+            PATHS.clientSrc, 
+            PATHS.hibachiSrc, 
+            PATHS.nodeModeles 
+        ],
+        alias       : { 
+            '@Monat': PATHS.monatFrontend, 
+            '@Hibachi': PATHS.hibachiSrc 
+        }
+    }
+};
+
+devConfig.module = {
+    rules: [
+		{
+			test: /\.tsx?$/i,
+			use: [
+				{
+					loader: 'cache-loader'
+				},
+                {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                        happyPackMode: true, // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+					    experimentalWatchApi: true
+                    }
+                }
+            ],
+            exclude: /node_modules/
+		}
+    ]
+};
+
+devConfig.optimization = {
+    splitChunks : {
+        cacheGroups: {
+            commons: {
+                test: PATHS.nodeModeles,
+                name: 'monatCommon',
+                chunks: 'all'
+            },
+            hibachi: {
+                test: PATHS.hibachiSrc,
+                name: 'hibachi',
+                chunks: 'all'
+            }
+        }
+    }
+};
 
 //don't need the vendor bundle generated here because we include the vendor bundle already.
 devConfig.plugins =  [
-    new CompressionPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8
+    
+	// https://blog.johnnyreilly.com/2016/07/using-webpacks-defineplugin-with-typescript.html
+    new webpack.DefinePlugin({
+        '__MONAT_FRONTEND_BASE_PATH__': JSON.stringify(PATHS.app),
+        '__DEBUG_MODE__': JSON.stringify(true)
     }),
-    new webpack.optimize.OccurrenceOrderPlugin()
+
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+    
+    new NgAnnotateWebPackPlugin({
+        add: true,
+        // other ng-annotate options here
+    }),
+    
+    new CompressionPlugin({
+      test: /\.(j|c)ss?$/i,
+      threshold: 10240,
+      filename: '[path].gz[query]',
+      deleteOriginalAssets: false
+    }),
+    
+    new ForkTsCheckerWebpackPlugin({
+        async: false,
+        useTypescriptIncrementalApi: true,
+        checkSyntacticErrors: true,
+        memoryLimit: 4096,
+        tsconfig: path.resolve(PATHS.clientRoot, 'tsconfig.json')
+    }),
+    
+    new ProgressBarPlugin()
 ];   
 
-devConfig.resolve.modules= [
-    path.resolve(path.join(customPath, './'), 'src/'),
-    path.resolve(path.join(customPath, '../../admin/client'), 'src/'),
-    path.resolve(path.join(customPath, '../../client'), 'src/'),
-    path.resolve(__dirname, 'src/'),
-    'node_modules'
-];
-
-devConfig.resolve.alias =  {
-      '@Monat': path.resolve(path.join(customPath, './'), 'src/monatfrontend/'),
-      '@Hibachi': path.resolve(path.join(customPath, '../../org/Hibachi/client/'), 'src/')
-};
-
 module.exports = devConfig;
+
