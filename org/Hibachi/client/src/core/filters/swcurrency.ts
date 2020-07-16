f34e0834c8cf7cfda6abfc69d61ee9e9455b1254
Tom Rodrick <tom.rodrick@ten24web.com>
2020-07-16 14:14:23 +0000

    debug

--- org/Hibachi/client/src/core/filters/swcurrency.ts
+++ org/Hibachi/client/src/core/filters/swcurrency.ts
@@ -9,13 +9,16 @@ class SWCurrency{
         var data = null, serviceInvoked = false;
         
         function realFilter(value, currencyCode:string, decimalPlace:number = 2, returnStringFlag = true) {
-
+            console.log('lineeeeeee 12', value)
+  
             if( isNaN( parseFloat(value) )  ){
+                console.log('returning')
                 return returnStringFlag ? "--" : undefined; 
             } 
-            
+            if(value == '')
             value = $filter('number')(value.toString(), decimalPlace);
-
+            console.log('line 18 value', value);
+       
             if(returnStringFlag){
                 var currencySymbol = "$";
                 if(data != null && data[currencyCode] != null ){
@@ -24,6 +27,7 @@ class SWCurrency{
                 else {
                      $log.debug("Please provide a valid currencyCode, swcurrency defaults to $");
                 }
+                console.log('line 27 value', value)
                 return currencySymbol + value; 
             }
             
@@ -31,6 +35,8 @@ class SWCurrency{
         }
 
         var filterStub: any = function(value, currencyCode:string, decimalPlace:number, returnStringFlag =true) {
+            console.log(' =======entry point ==============!', value)
+           
             if( data == null && returnStringFlag) {
 
                 if( !serviceInvoked ) {
@@ -39,13 +45,14 @@ class SWCurrency{
                         data = currencies.data;
                     });
                 }
+                console.log('line 47', value)
                 return  "--" + value;
             }
             else {
                 return realFilter(value,currencyCode,decimalPlace,returnStringFlag);
             }
         }
-
+        
         filterStub.$stateful = true;
 
         return filterStub;
