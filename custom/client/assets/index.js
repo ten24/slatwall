b41e9687f4edccf241d43a00fb2566fc6c82e910
Nitin Yadav <nitin.yadav@ten24web.com>
2020-09-18 11:46:47 +0000

    Add: webpack build-process for custom-client-assets

---
+++ custom/client/assets/index.js
@@ -0,0 +1,9 @@
+// TODO: replace these with npm-packahes
+
+// //css
+// import "./css/tiny-slider.min.css";
+
+// //js
+// import "./js/bootstrap.min.js";
+// import "./js/tiny-slider.min.js";
+// import "./js/theme.js";
