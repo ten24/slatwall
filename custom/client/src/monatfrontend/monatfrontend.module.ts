5e04e443d636f6bf40c0f729b3e34cf07a90e195
Miguel Targa <miguel.targa@ten24web.com>
2020-10-28 22:08:19 +0000

    Revert changes

--- custom/client/src/monatfrontend/monatfrontend.module.ts
+++ custom/client/src/monatfrontend/monatfrontend.module.ts
@@ -265,7 +265,21 @@ var monatfrontendmodule = angular
 			}
 		},
 	]);
-	
+	// the __DEBUG_MODE__ is driven by webpack-config and only enabled in debug-builds
+	if(__DEBUG_MODE__){
+	    // added here for debugging angular-bootstrapping, and other similar errors
+	    // this will throw all of the angular-exceptions 
+	    //   regardless if they're catched-anywhere ( .catch( error => () ) blocks )
+	    //   and you'll see a lot-more errors in the console
+	    //   this will effect all modules, as $exceptionHandler is part of angular-core
+        monatfrontendmodule.factory('$exceptionHandler', () => {
+            return (exception, cause) => {
+                exception.message += ` caused by '${cause || "no cause given"}' `;
+                // can log to sentry from here as well 
+                throw exception;
+            };
+        })
+    }
 	
 
 export { 
