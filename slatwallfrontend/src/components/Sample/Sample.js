4d777a61e6eb6439f01fe2c5f358ba9e50da43a3
Tom Smith <tasmith039@gmail.com>
2020-11-15 14:03:41 -0500

    Multiple Dom Elements
    This is the most simple Approach for Multiple render doms and a shared Store.

---
+++ slatwallfrontend/src/components/Sample/Sample.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Sample = ({ props }) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Sample </h1>
+    </div>
+  )
+}
+
+export default Sample
--- slatwallfrontend/src/index.js
+++ slatwallfrontend/src/index.js
@@ -1,23 +1,54 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
-import { App } from "./components/index"
+import { App, SWSlider } from "./components/index"
 import reportWebVitals from "./reportWebVitals"
 import "./styles/slatwall.scss"
 import { Provider } from "react-redux"
 import { store, persistor } from "./store"
 import { PersistGate } from "redux-persist/integration/react"
+const isMulti = true
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+if (isMulti) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <div>
+            <h1>Home 1 </h1>
+            <SWSlider />
+          </div>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("multi1")
+  )
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <div>
+            <h1>Home 2 </h1>
+            <SWSlider />
+          </div>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("multi2")
+  )
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
