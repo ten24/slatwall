4294fecb33cdffd5a8c0e27ee92a8fb715480596
Tom Smith <tasmith039@gmail.com>
2020-11-20 13:22:25 -0500

    remove files

--- custom/react/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as App } from './App/App'
-export { default as Navigation } from './Navigation/Navigation'
-export { default as SWSlider } from './Slider/Slider'
-export { default as Product } from './Product/Product'
-export { default as CartLineItem } from './CartLineItem/CartLineItem'
-export { default as Layout } from './Layout/Layout'
-export { default as AuthorFilter } from './AuthorFilter/AuthorFilter'
-export { default as HookSample } from './HookSample/HookSample'
--- custom/react/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import shop from './shop.reducer'
-import filter from './filter.reducer'
-
-export default combineReducers({
-  shop,
-  filter,
-})
