78089fe3689fc1bba596965aaccd13954a863dc1
Tom Smith <tasmith039@gmail.com>
2021-01-22 11:12:26 -0500

    update hompage to use not content

--- custom/swreact/src/pages/Home/Home.js
+++ custom/swreact/src/pages/Home/Home.js
@@ -1,11 +1,18 @@
 import React, { useEffect } from 'react'
 import { HomeBanner, HomeBrand, HomeDetails, Layout } from '../../components'
 import { useDispatch } from 'react-redux'
-import { getHomePageContent } from '../../actions/contentActions'
+import { getHomePageContent, getContent } from '../../actions/contentActions'
 const Home = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getHomePageContent())
+    dispatch(
+      getContent({
+        content: {
+          'home/': ['title', 'customBody', 'contentID', 'urlTitlePath', 'urlTitle', 'sortOrder', 'linkUrl', 'linkLabel'],
+        },
+      })
+    )
   }, [dispatch])
   return (
     <Layout>
