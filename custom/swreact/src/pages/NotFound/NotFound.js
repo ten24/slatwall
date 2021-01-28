2d884a37232b63b34ef0981b386ead4b2b312826
Erin Mooney <erin.mooney@ten24web.com>
2021-01-28 20:26:13 +0000

    https://ten24.teamwork.com/#/tasks/31393084

---
+++ custom/swreact/src/pages/NotFound/NotFound.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react'
+// import PropTypes from 'prop-types'
+import { Layout } from '../../components'
+import { connect, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { getContent } from '../../actions/contentActions'
+
+const NotFound = ({ title, customBody, customSummary }) => {
+  const dispatch = useDispatch()
+
+  let history = useHistory()
+  useEffect(() => {
+    dispatch(
+      getContent({
+        content: {
+          notfound: ['customBody', 'customSummary', 'title'],
+        },
+      })
+    )
+  }, [dispatch])
+  return (
+    <Layout>
+      <div class="container py-5 mb-lg-3">
+      <div class="row justify-content-center pt-lg-4 text-center">
+        <div class="col-lg-5 col-md-7 col-sm-9">
+          <h1 class="display-404">{title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: customSummary,
+            }}
+          ></div>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-xl-8 col-lg-10">
+          <div
+            onClick={event => {
+              event.preventDefault()
+              history.push(event.target.getAttribute('href'))
+            }}
+            dangerouslySetInnerHTML={{
+              __html: customBody,
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+    </Layout>
+  )
+}
+
+function mapStateToProps(state) {
+  return { ...state.content.notfound }
+}
+
+NotFound.propTypes = {}
+export default connect(mapStateToProps)(NotFound)
