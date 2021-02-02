b6e6992b4ab05de2b2f2bd4fb64671fc5eafe6d5
Tom Smith <tasmith039@gmail.com>
2021-02-02 15:08:24 -0500

    Build bundle

---
+++ custom/swreact/build/static/js/20.a545cf45.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpswreact=this.webpackJsonpswreact||[]).push([[20],{271:function(t,e,c){"use strict";c.r(e);var s=c(0),n=c(1),r=c(24),i=c(8),a=c(46);e.default=function(){var t=Object(i.c)();return Object(n.useEffect)((function(){t(Object(a.d)()),t(Object(a.c)({content:{home:["title","customBody","contentID","urlTitlePath","urlTitle","sortOrder","linkUrl","linkLabel","associatedImage"]}}))}),[t]),Object(s.jsxs)(r.i,{children:[Object(s.jsx)(r.f,{}),Object(s.jsx)(r.h,{}),Object(s.jsx)(r.g,{})]})}}}]);
+//# sourceMappingURL=20.a545cf45.chunk.js.map
