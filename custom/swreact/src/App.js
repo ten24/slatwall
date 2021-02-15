5c157aba98c6337b2052c91eeb9b2968e370e637
Tom Smith <tasmith039@gmail.com>
2021-02-15 11:45:17 -0500

    make the router dynamic

--- custom/swreact/src/App.js
+++ custom/swreact/src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense, useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import { Header, Layout, SEO } from './components'
 import lazyWithPreload from './components/lazyWithPreload/lazyWithPreload'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
@@ -11,58 +12,61 @@ const ProductDetail = lazyWithPreload(() => import('./pages/ProductDetail/Produc
 
 const CategoryListing = lazyWithPreload(() => import('./pages/CategoryListing/CategoryListing'))
 
-const Contact = lazyWithPreload(() => import('./pages/Contact/Contact'))
-
-const About = lazyWithPreload(() => import('./pages/About/About'))
-
 const Testing = lazyWithPreload(() => import('./pages/Testing/Testing'))
 const Brand = lazyWithPreload(() => import('./pages/Brand/Brand'))
 
-const NotFound = React.lazy(() => import('./pages/NotFound/NotFound'))
-
+const NotFound = lazyWithPreload(() => import('./pages/NotFound/NotFound'))
+const ContentPage = lazyWithPreload(() => import('./pages/ContentPage/ContentPage'))
+const pageComponents = {
+  Home,
+  MyAccount,
+  ProductListing,
+  ProductDetail,
+  Testing,
+  Brand,
+  NotFound,
+  ProductType: NotFound,
+  Category: NotFound,
+  Account: MyAccount,
+  Address: NotFound,
+  Attribute: NotFound,
+  ContentPage,
+}
 const Loading = () => {
   return <Layout></Layout>
 }
 //https://itnext.io/react-router-transitions-with-lazy-loading-2faa7a1d24a
 export default function App() {
+  const routing = useSelector(state => state.preload.router)
   useEffect(() => {
-    Home.preload()
-    MyAccount.preload()
-    ProductListing.preload()
-    ProductDetail.preload()
-    CategoryListing.preload()
-    Contact.preload()
-    About.preload()
-    Testing.preload()
-    Brand.preload()
-  }, [Home, About, Brand])
+    Object.keys(pageComponents).map(key => {
+      return pageComponents[key].preload()
+    })
+  }, [])
 
   return (
-    <Router>
+    <Suspense fallback={<Loading />}>
       <ScrollToTop />
-      <Suspense fallback={<Loading />}>
-        <SEO />
-        <Header />
-        {/* TODO: We need a spinner */}
-        {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/products" component={ProductListing} />
-          <Route path="/product/:id" component={ProductDetail} />
-          <Route path="/product" component={ProductListing} />
-          <Route path="/sp/:id" component={ProductDetail} />
-          <Route path="/shop/:id" component={Brand} />
-          <Route path="/brand/:id" component={Brand} />
-          <Route path="/category-listing" component={CategoryListing} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/my-account" component={MyAccount} />
-          <Route path="/MyAccount" component={MyAccount} />
-          <Route path="/testing" component={Testing} />
-          <Route exact path="/" component={Home} />
-          <Route path="" component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+      <SEO />
+      <Header />
+      <Switch>
+        {routing.length &&
+          routing.map(({ URLKey, URLKeyType }, index) => {
+            return <Route key={index} path={`/${URLKey}/:id`} component={pageComponents[URLKeyType]} />
+          })}
+        <Route path="/products" component={ProductListing} />
+        <Route path="/product/:id" component={ProductDetail} />
+        <Route path="/product" component={ProductListing} />
+        <Route path="/sp/:id" component={ProductDetail} />
+        <Route path="/shop/:id" component={Brand} />
+        <Route path="/brand/:id" component={Brand} />
+        <Route path="/category-listing" component={CategoryListing} />
+        <Route path="/my-account" component={MyAccount} />
+        <Route path="/MyAccount" component={MyAccount} />
+        <Route path="/testing" component={Testing} />
+        <Route exact path="/" component={Home} />
+        <Route path="" component={ContentPage} />
+      </Switch>
+    </Suspense>
   )
 }
