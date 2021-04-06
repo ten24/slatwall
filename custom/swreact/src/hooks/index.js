23fafd589e3756b108a2fb2e2aeb473d8d9bba8b
Tom Smith <tasmith039@gmail.com>
2021-04-06 09:19:38 -0400

    update for library

---
+++ custom/swreact/src/hooks/index.js
@@ -0,0 +1,6 @@
+import { useGetSku, useGetProductDetails, useGetBrand, useGetBrands, useGetProductList, useGetProducts, useGetAvailableShippingMethods, useGetAvailablePaymentMethods, useAddWishlistItem, useGetOrderDetails, useGetAllOrders, useAddOrderShippingAddress, useGetFavouriteProducts, useGetSkuList, useGetProductSkus, useGetProductAvailableSkuOptions, useGetProductSkuSelected, useGetProductImageGallery } from './useAPI'
+import useFormatCurrency from './useFormatCurrency'
+import { useFormatDate, useFormatDateTime, useFormatTime } from './useFormatDate'
+import { useRedirect, usePush } from './useRedirect'
+
+export { useGetSku, useGetProductDetails, useGetBrand, useGetBrands, useGetProductList, useGetProducts, useGetAvailableShippingMethods, useGetAvailablePaymentMethods, useAddWishlistItem, useGetOrderDetails, useGetAllOrders, useAddOrderShippingAddress, useGetFavouriteProducts, useGetSkuList, useGetProductSkus, useGetProductAvailableSkuOptions, useGetProductSkuSelected, useGetProductImageGallery, useFormatCurrency, useFormatDate, useFormatDateTime, useFormatTime, useRedirect, usePush }
