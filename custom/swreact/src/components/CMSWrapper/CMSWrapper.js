9cca8b9a240209455afb47837e7d8d903cf5b3e9
Tom Smith <tasmith039@gmail.com>
2021-04-07 11:35:30 -0400

    add

--- custom/swreact/src/components/CMSWrapper/CMSWrapper.js
+++ custom/swreact/src/components/CMSWrapper/CMSWrapper.js
@@ -4,10 +4,12 @@ import { getContent } from '../../actions/contentActions'
 import { useHistory, useLocation } from 'react-router'
 import { getFavouriteProducts } from '../../actions/userActions'
 
+const basicProperties = ['title', 'customSummary', 'customBody', 'contentID', 'urlTitlePath', 'urlTitle', 'sortOrder', 'linkUrl', 'linkLabel', 'associatedImage', 'parentContentID', 'displayInNavigation']
+
 let payload = {
-  header: ['title', 'customSummary', 'customBody', 'contentID', 'urlTitlePath', 'urlTitle', 'sortOrder', 'linkUrl', 'linkLabel', 'associatedImage', 'parentContentID'],
-  footer: ['title', 'customSummary', 'customBody', 'contentID', 'urlTitlePath', 'urlTitle', 'sortOrder', 'linkUrl', 'linkLabel', 'associatedImage', 'parentContentID'],
-  '404': ['title', 'customSummary', 'customBody', 'contentID', 'urlTitlePath', 'urlTitle', 'sortOrder', 'linkUrl', 'linkLabel', 'associatedImage', 'parentContentID'],
+  header: basicProperties,
+  footer: basicProperties,
+  '404': basicProperties,
 }
 
 const CMSWrapper = ({ children }) => {
@@ -16,9 +18,13 @@ const CMSWrapper = ({ children }) => {
   const history = useHistory()
 
   let path = pathname.split('/').reverse()[0].toLowerCase()
+  let basePath = pathname.split('/')[1].toLowerCase()
   path = path.length ? path : 'home'
-
-  payload[path] = ['title', 'customSummary', 'customBody', 'contentID', 'urlTitlePath', 'urlTitle', 'sortOrder', 'linkUrl', 'linkLabel', 'associatedImage', 'parentContentID']
+  basePath = basePath.length ? basePath : 'home'
+  if (path !== basePath) {
+    payload[basePath] = basicProperties
+  }
+  payload[path] = basicProperties
 
   useEffect(() => {
     dispatch(getFavouriteProducts())
@@ -31,7 +37,7 @@ const CMSWrapper = ({ children }) => {
       let NewPayload = {}
       let newPath = location.pathname.split('/').reverse()[0].toLowerCase()
       newPath = newPath.length ? newPath : 'home'
-      NewPayload[newPath] = ['title', 'customSummary', 'customBody', 'contentID', 'urlTitlePath', 'urlTitle', 'sortOrder', 'linkUrl', 'linkLabel', 'associatedImage', 'parentContentID']
+      NewPayload[newPath] = basicProperties
       dispatch(getFavouriteProducts())
       dispatch(
         getContent({
