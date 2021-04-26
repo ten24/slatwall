e99685cd5903e8c7a5d98979ed230a9bc14d18a7
Tom Smith <tasmith039@gmail.com>
2021-04-26 11:09:21 -0400

    https://ten24.teamwork.com/#/tasks/32249009

--- custom/swreact/src/pages/Contact/Contact.js
+++ custom/swreact/src/pages/Contact/Contact.js
@@ -1,7 +1,12 @@
 import BasicPageWithSidebar from '../BasicPageWithSidebar/BasicPageWithSidebar'
+import ContactForm from './ContactForm'
 
 const Contact = () => {
-  return <BasicPageWithSidebar />
+  return (
+    <BasicPageWithSidebar>
+      <ContactForm />
+    </BasicPageWithSidebar>
+  )
 }
 
 export default Contact
