a87b8e96c0ca79340ffc4b906c04e92f1849ec28
Abhishek Sharma <abhishek.sharma@slatwallcommerce.com>
2021-05-04 12:55:02 +0000

    Setting Alerts when there is no address or payment methods applied https://ten24.teamwork.com/#/tasks/32288880

--- custom/swreact/src/components/Account/AccountContent/AccountContent.js
+++ custom/swreact/src/components/Account/AccountContent/AccountContent.js
@@ -1,19 +1,32 @@
 import { useSelector } from 'react-redux'
 import { useHistory, useLocation } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { getAllAccountAddresses } from '../../../selectors/userSelectors'
+
 
 const AccountContent = () => {
+  const { t } = useTranslation()
   let history = useHistory()
   let loc = useLocation()
   const content = useSelector(state => state.content[loc.pathname.substring(1)])
+  const { accountPaymentMethods } = useSelector(state => state.userReducer)
+  const accountAddresses = useSelector(getAllAccountAddresses)
   const { customBody = '', contentTitle = '' } = content || {}
+ 
+  let alertMesage = (accountPaymentMethods.length === 0 && accountAddresses.length === 0) ? 'frontend.account.missing_address_paymentMethod' :
+                    accountPaymentMethods.length === 0 ? 'frontend.account.payment_methods.none' :
+                    accountAddresses.length === 0 ? 'frontend.account.address.none' :
+                    ''
   return (
     <>
       <div className="d-flex justify-content-between align-items-center pt-lg-2 pb-4 pb-lg-5 mb-lg-3">
         <div className="d-flex justify-content-between w-100">
-          <h2 className="h3">{contentTitle}</h2>
+          <div className="alert alert-info" role="alert">
+            { t(alertMesage) }
+         </div>
         </div>
       </div>
-
+      
       <div
         onClick={event => {
           event.preventDefault()
