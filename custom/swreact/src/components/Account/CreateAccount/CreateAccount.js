ce5e08550d6e36518f0145b7b34710e4e1fdb5b0
Tom Smith <tasmith039@gmail.com>
2021-05-05 22:16:01 -0400

    https://ten24.teamwork.com/#/tasks/32286200

--- custom/swreact/src/components/Account/CreateAccount/CreateAccount.js
+++ custom/swreact/src/components/Account/CreateAccount/CreateAccount.js
@@ -5,18 +5,25 @@ import { SWForm, SWInput } from '../../SWForm/SWForm'
 import { PromptLayout } from '../AccountLayout/AccountLayout'
 import useRedirect from '../../../hooks/useRedirect'
 import * as Yup from 'yup'
+import { getErrorMessage } from '../../../utils'
+import { useDispatch } from 'react-redux'
+import { updateToken } from '../../../actions/authActions'
 
 const CreateAccount = () => {
+  const dispatch = useDispatch()
+  const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
   // eslint-disable-next-line no-unused-vars
   const [redirect, setRedirect] = useRedirect({ location: '/my-account' })
   const signupSchema = Yup.object().shape({
     firstName: Yup.string().required('Required'),
     lastName: Yup.string().required('Required'),
-    phoneNumber: Yup.string().required('Required'),
+    phoneNumber: Yup.string().required('Required').matches(phoneRegExp, 'Phone number is not valid'),
     password: Yup.string().required('Required'),
-    passwordConfirm: Yup.string().required('Required'),
+    passwordConfirm: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match'),
     emailAddress: Yup.string().email('Invalid email').required('Required'),
-    emailAddressConfirm: Yup.string().email('Invalid email').required('Required'),
+    emailAddressConfirm: Yup.string()
+      .email('Invalid email')
+      .oneOf([Yup.ref('emailAddress'), null], 'Emails must match'),
   })
 
   const formik = useFormik({
@@ -31,21 +38,24 @@ const CreateAccount = () => {
       password: '',
       passwordConfirm: '',
     },
+    validateOnChange: false,
     validationSchema: signupSchema,
     onSubmit: values => {
       SlatwalApiService.account.create(values).then(response => {
         if (response.isSuccess()) {
           if (!response.success().failureActions.length) {
             toast.success('Success')
+            dispatch(updateToken(response.success()))
             setRedirect({ shouldRedirect: true })
           }
-          toast.error(JSON.stringify(response.success().errors))
+          toast.error(getErrorMessage(response.success().errors))
         } else {
-          toast.success('Error')
+          toast.error('Error')
         }
       })
     },
   })
+
   return (
     <PromptLayout>
       <SWForm formik={formik} primaryButtontext="Create Account & Continue" title="Create Account">
