102575cbc6a8621a963a43cb144e924acab29b70
Tom Smith <tasmith039@gmail.com>
2021-05-12 09:32:13 -0400

    https://ten24.teamwork.com/#/tasks/32490861

--- custom/swreact/src/services/SlatwalApiService.js
+++ custom/swreact/src/services/SlatwalApiService.js
@@ -1,9 +1,18 @@
 import * as SlatwalSDK from '@slatwall/slatwall-sdk/dist/client/index'
 // import config from '../config/env'
+import store from '../createStore'
 
 const sdkURL = process.env.REACT_APP_API_URL
 
 let SlatwalApiService = SlatwalSDK.init({
   host: sdkURL,
+})
+SlatwalApiService.sdkScope.httpService.axios.interceptors.request.use(config => {
+  const siteCode = store.getState().configuration.site.siteCode
+  config.headers['SWX-requestSiteCode'] = siteCode
+  if (localStorage.getItem('token')) {
+    config.headers['Auth-Token'] = `Bearer ${localStorage.getItem('token')}`
+  }
+  return config
 })
 export { sdkURL, SlatwalApiService }
