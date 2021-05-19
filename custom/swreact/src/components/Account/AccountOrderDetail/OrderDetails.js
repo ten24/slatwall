f67aeba9771ea08e6be2765120cd415e1cc4a39a
Tom Smith <tasmith039@gmail.com>
2021-05-19 13:23:38 -0400

    add diplay

--- custom/swreact/src/components/Account/AccountOrderDetail/OrderDetails.js
+++ custom/swreact/src/components/Account/AccountOrderDetail/OrderDetails.js
@@ -4,9 +4,9 @@ import { ShippingAddressDetails, BillingAddressDetails, TermPaymentDetails, Gift
 
 const OrderDetails = ({ orderInfo, orderFulfillments, orderPayments }) => {
   const { orderOpenDateTime } = orderInfo
-  const { orderFulfillment_fulfillmentMethod_fulfillmentMethodType, orderFulfillment_shippingAddress_name, orderFulfillment_shippingAddress_streetAddress, orderFulfillment_shippingAddress_city, orderFulfillment_shippingAddress_stateCode, orderFulfillment_shippingAddress_postalCode, orderFulfillment_pickupLocation_locationName } = orderFulfillments
+  const { orderFulfillment_shippingAddress_emailAddress, orderFulfillment_fulfillmentMethod_fulfillmentMethodType, orderFulfillment_shippingAddress_name, orderFulfillment_shippingAddress_streetAddress, orderFulfillment_shippingAddress_city, orderFulfillment_shippingAddress_stateCode, orderFulfillment_shippingAddress_postalCode, orderFulfillment_pickupLocation_locationName } = orderFulfillments
   const { calculatedTotal, calculatedSubTotal, calculatedTaxTotal, calculatedFulfillmentTotal, calculatedDiscountTotal } = orderInfo
-  const { paymentMethod_paymentMethodType, paymentMethod_paymentMethodName, creditCardLastFour, billingAddress_streetAddress, billingAddress_city, billingAddress_stateCode, billingAddress_postalCode, billingAddress_name, purchaseOrderNumber, nameOnCreditCard, creditCardType } = orderPayments
+  const { paymentMethod_paymentMethodType, paymentMethod_paymentMethodName, creditCardLastFour, billingAddress_streetAddress, billingAddress_emailAddress, billingAddress_city, billingAddress_stateCode, billingAddress_postalCode, billingAddress_name, purchaseOrderNumber, nameOnCreditCard, creditCardType } = orderPayments
   const [formateDate] = useFormatDateTime({})
   const [formatCurrency] = useFormatCurrency({})
 
@@ -15,13 +15,13 @@ const OrderDetails = ({ orderInfo, orderFulfillments, orderPayments }) => {
       <div className="col-md-7">
         <div className="row text-sm">
           <div className="col-6 ">
-            {orderFulfillment_fulfillmentMethod_fulfillmentMethodType === 'shipping' && <ShippingAddressDetails className="" shippingAddress={{ name: orderFulfillment_shippingAddress_name, streetAddress: orderFulfillment_shippingAddress_streetAddress, city: orderFulfillment_shippingAddress_city, stateCode: orderFulfillment_shippingAddress_stateCode, postalCode: orderFulfillment_shippingAddress_postalCode }} shippingAddressNickname={''} />}
+            {orderFulfillment_fulfillmentMethod_fulfillmentMethodType === 'shipping' && <ShippingAddressDetails className="" shippingAddress={{ name: orderFulfillment_shippingAddress_name, streetAddress: orderFulfillment_shippingAddress_streetAddress, city: orderFulfillment_shippingAddress_city, emailAddress: orderFulfillment_shippingAddress_emailAddress, stateCode: orderFulfillment_shippingAddress_stateCode, postalCode: orderFulfillment_shippingAddress_postalCode }} shippingAddressNickname={''} />}
             {orderFulfillment_fulfillmentMethod_fulfillmentMethodType === 'pickup' && <PickupLocationDetails className="" pickupLocation={{ locationName: orderFulfillment_pickupLocation_locationName }} />}
             <h3 className="h6">Date Placed</h3>
             <p>{formateDate(orderOpenDateTime)}</p>
           </div>
           <div className="col-6">
-            <BillingAddressDetails billingAddressNickname={''} orderPayment={{ billingAddress: { name: billingAddress_name, streetAddress: billingAddress_streetAddress, city: billingAddress_city, stateCode: billingAddress_stateCode, postalCode: billingAddress_postalCode } }} />
+            <BillingAddressDetails billingAddressNickname={''} orderPayment={{ billingAddress: { name: billingAddress_name, streetAddress: billingAddress_streetAddress, city: billingAddress_city, stateCode: billingAddress_stateCode, postalCode: billingAddress_postalCode, emailAddress: billingAddress_emailAddress } }} />
             {paymentMethod_paymentMethodType === 'termPayment' && <TermPaymentDetails termPayment={{ purchaseOrderNumber: purchaseOrderNumber, paymentMethod: { paymentMethodName: paymentMethod_paymentMethodName } }} />}
             {paymentMethod_paymentMethodType === 'giftCard' && <GiftCardDetails />}
             {paymentMethod_paymentMethodType === 'creditCard' && <CreditCardDetails creditCardPayment={{ creditCardType, nameOnCreditCard, creditCardLastFour, paymentMethod: { paymentMethodName: paymentMethod_paymentMethodName } }} />}
