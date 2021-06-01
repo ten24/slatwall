07bca86cdab278db2accade61c563b24645f9b06
Erin Mooney <erin.mooney@ten24web.com>
2021-06-01 19:34:10 +0000

    https://ten24.teamwork.com/#tasks/32548979

--- custom/swreact/src/pages/Brand/BrandBanner.js
+++ custom/swreact/src/pages/Brand/BrandBanner.js
@@ -1,19 +1,17 @@
 import { SWImage } from '../../components'
 const BrandBanner = ({ brandName = '', imageFile, brandDescription, subHeading }) => {
   return (
-    <div className="container pt-3 justify-content-between">
-      <div className="row align-items-center">
-        {/* Checking 'imageFile' length so that empty string does not get passed */}
-        {imageFile && imageFile.length > 5 && <SWImage style={{ maxHeight: '150px', marginRight: '30px' }} customPath="/custom/assets/images/brand/logo/" src={imageFile} alt={brandName} type="brand" />}
-        <div className="col text-left">
-          <h1>{brandName}</h1>
-          {!!subHeading && <h3>{subHeading}</h3>}
-        </div>
+    <div className="row align-items-center">
+      {/* Checking 'imageFile' length so that empty string does not get passed */}
+      {imageFile && imageFile.length > 5 && <SWImage style={{ maxHeight: '150px' }} customPath="/custom/assets/images/brand/logo/" src={imageFile} alt={brandName} type="brand" className="bg-white mr-2 ml-3" />}
+      <div className="col text-left">
+        <h2 className="m-0 font-weight-normal">{brandName}</h2>
+        {!!subHeading && <h3 className="h5 m-0">{subHeading}</h3>}
+      </div>
 
-        {/* <span dangerouslySetInnerHTML={{ __html: brandDescription }} /> */}
-        <div className="col text-right ">
-          <span dangerouslySetInnerHTML={{ __html: brandDescription }} />
-        </div>
+      {/* <span dangerouslySetInnerHTML={{ __html: brandDescription }} /> */}
+      <div className="col text-right ">
+        <span dangerouslySetInnerHTML={{ __html: brandDescription }} />
       </div>
     </div>
   )
