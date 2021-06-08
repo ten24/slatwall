09163f7b97d652406dc0cdd4219727594011bcf4
Tom Smith <tasmith039@gmail.com>
2021-06-08 16:46:07 -0400

    fix tema nd credit card

---
+++ custom/swreact/src/components/Checkout/Fulfilment/ShippingMethodPicker.js
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { addShippingMethod } from '../../../actions/cartActions'
+import { SwRadioSelect } from '../../../components'
+import { fulfillmentSelector, shippingMethodSelector } from '../../../selectors/orderSelectors'
+import { useTranslation } from 'react-i18next'
+
+const ShippingMethodPicker = () => {
+  const dispatch = useDispatch()
+  const orderFulfillments = useSelector(state => state.cart.orderFulfillments)
+  const selectedShippingMethod = useSelector(shippingMethodSelector)
+  const orderFulfillment = useSelector(fulfillmentSelector)
+  const { t } = useTranslation()
+
+  return (
+    <div className="row mb-3">
+      <div className="col-sm-12">
+        {orderFulfillments.length > 0 && (
+          <SwRadioSelect
+            label={t('frontend.checkout.delivery_option')}
+            options={orderFulfillment.shippingMethodOptions}
+            onChange={value => {
+              dispatch(
+                addShippingMethod({
+                  shippingMethodID: value,
+                  fulfillmentID: orderFulfillment.orderFulfillmentID,
+                })
+              )
+            }}
+            selectedValue={selectedShippingMethod.shippingMethodID}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export { ShippingMethodPicker }
