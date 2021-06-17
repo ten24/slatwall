27a9e007df4b50cf69f78c8fc88f57b7bb5c3b0b
Tom Smith <tasmith039@gmail.com>
2021-06-16 22:20:15 -0400

    inital sentry integration

--- custom/swreact/src/index.js
+++ custom/swreact/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import reportWebVitals from './reportWebVitals'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
 import './i18n'
 import { Provider } from 'react-redux'
@@ -10,9 +11,25 @@ import App from './App'
 import './assets/theme'
 import TagManager from 'react-gtm-module'
 import devData from './preload'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+const history = createBrowserHistory()
 
 TagManager.initialize({
   gtmId: devData.analytics.tagManager.gtmId,
+})
+Sentry.init({
+  dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
+  release: 'StoneAndBerg@' + process.env.npm_package_version,
+  integrations: [
+    new Integrations.BrowserTracing({
+      // Can also use reactRouterV3Instrumentation or reactRouterV4Instrumentation
+      routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+    }),
+  ],
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
 })
 
 ReactDOM.render(
